cea92be8b1c1bd8cb875031b522f9593a3878aa5
khlifi411 <46890720+khlifi411@users.noreply.github.com>
2022-06-22 21:02:21 +0200

    removed unused label consts

--- operator/pkg/labels/operator_labels.go
+++ operator/pkg/labels/operator_labels.go
@@ -3,15 +3,12 @@ package labels
 import "github.com/kyma-project/kyma-operator/operator/api/v1alpha1"
 
 const (
-	SKREventsPrefix     = "skr.events.kyma-project.io"
-	ListenerLastUpdated = SKREventsPrefix + Separator + "last-updated"
-	ClusterName         = SKREventsPrefix + Separator + "cluster-name"
-	OperatorPrefix      = "operator.kyma-project.io"
-	ComponentPrefix     = "component.kyma-project.io"
-	Separator           = "/"
-	ControllerName      = OperatorPrefix + Separator + "controller-name"
-	Channel             = OperatorPrefix + Separator + "channel"
-	ManagedBy           = OperatorPrefix + Separator + "managed-by"
-	Finalizer           = OperatorPrefix + Separator + v1alpha1.KymaKind
-	ComponentOwner      = ComponentPrefix + Separator + "kyma-name"
+	OperatorPrefix  = "operator.kyma-project.io"
+	ComponentPrefix = "component.kyma-project.io"
+	Separator       = "/"
+	ControllerName  = OperatorPrefix + Separator + "controller-name"
+	Channel         = OperatorPrefix + Separator + "channel"
+	ManagedBy       = OperatorPrefix + Separator + "managed-by"
+	Finalizer       = OperatorPrefix + Separator + v1alpha1.KymaKind
+	ComponentOwner  = ComponentPrefix + Separator + "kyma-name"
 )
