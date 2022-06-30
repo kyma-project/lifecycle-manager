90aebaaf7d5b7453d110abe4ef50249e192b6465
Jakob Möller <jakob.moeller@sap.com>
2022-06-30 09:36:07 +0200

    feat: pr-review operator/controllers/kyma_controller.go
    Co-authored-by: Aditya Bhatia <aditya.bhatia@sap.com>

--- operator/controllers/kyma_controller.go
+++ operator/controllers/kyma_controller.go
@@ -322,7 +322,7 @@ func (r *KymaReconciler) CreateOrUpdateModules(ctx context.Context, kyma *operat
 				return false, err
 			}
 			status.Helper(r).SyncReadyConditionForModules(kyma, util.Modules{name: module},
-				operatorv1alpha1.ConditionStatusFalse, "initial condition for module cr")
+				operatorv1alpha1.ConditionStatusFalse, fmt.Sprintf("initial condition for %s module CR", module)
 			logger.Info("successfully created module CR of",
 				"type", name,
 				"templateChannel", module.Channel(),
