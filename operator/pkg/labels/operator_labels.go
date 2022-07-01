19de7117925b1f1d59a9c62931dd58a321600316
Aditya Bhatia <aditya.bhatia@sap.com>
2022-07-01 13:07:57 +0200

    add label to kyma CR

--- operator/pkg/labels/operator_labels.go
+++ operator/pkg/labels/operator_labels.go
@@ -1,6 +1,9 @@
 package labels
 
-import "github.com/kyma-project/kyma-operator/operator/api/v1alpha1"
+import (
+	"github.com/kyma-project/kyma-operator/operator/api/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
 
 const (
 	OperatorPrefix  = "operator.kyma-project.io"
@@ -13,3 +16,21 @@ const (
 	KymaName        = OperatorPrefix + Separator + "kyma-name"
 	LastSync        = OperatorPrefix + Separator + "last-sync"
 )
+
+func CheckLabelsAndFinalizers(kyma *v1alpha1.Kyma) bool {
+	updateRequired := false
+	if !controllerutil.ContainsFinalizer(kyma, Finalizer) {
+		controllerutil.AddFinalizer(kyma, Finalizer)
+		updateRequired = true
+	}
+
+	if kyma.ObjectMeta.Labels == nil {
+		kyma.ObjectMeta.Labels = make(map[string]string, 0)
+	}
+
+	if _, ok := kyma.ObjectMeta.Labels[ManagedBy]; !ok {
+		kyma.ObjectMeta.Labels[ManagedBy] = v1alpha1.KymaKind
+		updateRequired = true
+	}
+	return updateRequired
+}
