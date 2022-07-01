77462ed4772d00f20e4645ea977ba72b335221d8
Xin Ruan <xin.ruan@sap.com>
2022-07-01 16:05:56 +0200

    ignore godox

--- operator/pkg/remote/kyma_synchronization_context.go
+++ operator/pkg/remote/kyma_synchronization_context.go
@@ -162,6 +162,7 @@ func (c *KymaSynchronizationContext) SynchronizeRemoteKyma(ctx context.Context, 
 
 	if c.controlPlaneKyma.Status.ObservedGeneration != c.controlPlaneKyma.GetGeneration() {
 		// control plane got updated, runtime on cluster is using the wrong base instance for customization
+		//nolint:godox
 		// TODO this now requires custom merge logic, but for now we reapply the control plane version
 		remoteKyma.Spec = c.controlPlaneKyma.Spec
 	}
