c5f5d9b1aabd4359b78815997a632fc6604adef2
Aditya Bhatia <aditya.bhatia@sap.com>
2022-08-10 14:56:27 +0200

    rename custom resource - mockup -> sample

--- samples/template-operator/operator/main.go
+++ samples/template-operator/operator/main.go
@@ -78,11 +78,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controllers.MockupReconciler{
+	if err = (&controllers.SampleReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Mockup")
+		setupLog.Error(err, "unable to create controller", "controller", "Sample")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
