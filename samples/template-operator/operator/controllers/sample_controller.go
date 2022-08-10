633d2f7febd97b82d95dc8d09dbedce9997699a1
Aditya Bhatia <aditya.bhatia@sap.com>
2022-08-10 16:19:53 +0200

    add initial implementation for sample resource

--- samples/template-operator/operator/controllers/sample_controller.go
+++ samples/template-operator/operator/controllers/sample_controller.go
@@ -18,7 +18,13 @@ package controllers
 
 import (
 	"context"
+	"github.com/go-logr/logr"
 	"github.com/kyma-project/kyma-operator/samples/template-operator/api/v1alpha1"
+	"github.com/kyma-project/manifest-operator/operator/pkg/custom"
+	manifestLib "github.com/kyma-project/manifest-operator/operator/pkg/manifest"
+	"helm.sh/helm/v3/pkg/cli"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/rest"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -30,6 +36,7 @@ import (
 type SampleReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	*rest.Config
 }
 
 //+kubebuilder:rbac:groups=component.kyma-project.io,resources=samples,verbs=get;list;watch;create;update;patch;delete
@@ -61,13 +68,21 @@ func (r *SampleReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	// check if deletionTimestamp is set, retry until it gets fully deleted
+	if !sampleResource.DeletionTimestamp.IsZero() &&
+		sampleResource.Status.State != v1alpha1.SampleStateDeleting {
+		// if the status is not yet set to deleting, also update the status
+		sampleResource.Status.State = v1alpha1.SampleStateDeleting
+		return ctrl.Result{}, r.Status().Update(ctx, sampleResource)
+	}
+
 	switch sampleResource.Status.State {
 	case "":
 		return ctrl.Result{}, r.HandleInitialState(ctx, sampleResource)
 	case v1alpha1.SampleStateProcessing:
-		return ctrl.Result{}, r.HandleProcessingState(ctx, sampleResource)
+		return ctrl.Result{}, r.HandleProcessingState(ctx, sampleResource, &logger)
 	case v1alpha1.SampleStateDeleting:
-		return ctrl.Result{}, r.HandleDeletingState(ctx, sampleResource)
+		return ctrl.Result{}, r.HandleDeletingState(ctx, sampleResource, &logger)
 	case v1alpha1.SampleStateError:
 		return ctrl.Result{}, r.HandleErrorState(ctx, sampleResource)
 	case v1alpha1.SampleStateReady:
@@ -85,20 +100,120 @@ func (r *SampleReconciler) HandleInitialState(ctx context.Context, sampleResourc
 	return r.Client.Status().Update(ctx, sampleResource)
 }
 
-func (r *SampleReconciler) HandleProcessingState(ctx context.Context, sampleResource *v1alpha1.Sample) error {
+func (r *SampleReconciler) HandleProcessingState(ctx context.Context, sampleResource *v1alpha1.Sample, logger *logr.Logger) error {
 	// TODO: processing logic here
 
-	// Example: If Processing succeeds, set to Ready state
-	sampleResource.Status.State = v1alpha1.SampleStateReady
-	return r.Client.Status().Update(ctx, sampleResource)
+	sampleObjUnstructured, err := runtime.DefaultUnstructuredConverter.ToUnstructured(sampleResource)
+	if err != nil {
+		return err
+	}
+
+	manifestClient, err := r.getManifestClient(logger)
+	if err != nil {
+		sampleResource.Status.State = v1alpha1.SampleStateError
+		return r.Client.Status().Update(ctx, sampleResource)
+	}
+
+	// Use manifest library client to install a sample chart
+	ready, err := manifestClient.Install(manifestLib.InstallInfo{
+		Ctx: ctx,
+		ChartInfo: &manifestLib.ChartInfo{
+			ChartName:   "nginx-ingress",
+			URL:         "https://helm.nginx.com/stable",
+			RepoName:    "my-chart-repo",
+			ReleaseName: "sampleReleaseName",
+		},
+		RemoteInfo: custom.RemoteInfo{
+			// destination cluster rest config
+			RemoteConfig: r.Config,
+			// destination cluster rest client
+			RemoteClient: &r.Client,
+		},
+		ResourceInfo: manifestLib.ResourceInfo{
+			// base operator resource to be passed for custom checks
+			BaseResource: &unstructured.Unstructured{Object: sampleObjUnstructured},
+		},
+		CheckFn: func(context.Context, *unstructured.Unstructured, *logr.Logger, custom.RemoteInfo) (bool, error) {
+			// your custom logic here to set ready state
+			return true, nil
+		},
+		CheckReadyStates: false,
+	})
+
+	if err != nil {
+		return err
+	}
+	if ready {
+		sampleResource.Status.State = v1alpha1.SampleStateReady
+		return r.Client.Status().Update(ctx, sampleResource)
+	}
+	return nil
 }
 
-func (r *SampleReconciler) HandleDeletingState(ctx context.Context, sampleResource *v1alpha1.Sample) error {
+func (r *SampleReconciler) getManifestClient(logger *logr.Logger) (*manifestLib.Operations, error) {
+	// Example: Prepare manifest library client
+	return manifestLib.NewOperations(logger, r.Config, "sampleReleaseName", cli.New(),
+		map[string]map[string]interface{}{
+			// check --set flags parameter for helm
+			"set": {},
+			// comma separated values of manifest command line flags
+			"flags": {
+				"Namespace":       "sampleNs",
+				"CreateNamespace": true,
+			},
+		})
+}
+
+func (r *SampleReconciler) HandleDeletingState(ctx context.Context, sampleResource *v1alpha1.Sample,
+	logger *logr.Logger) error {
 	// TODO: deletion logic here
 
-	// Example: If Deleting state, remove Finalizers
-	sampleResource.Finalizers = nil
-	return r.Client.Update(ctx, sampleResource)
+	sampleObjUnstructured, err := runtime.DefaultUnstructuredConverter.ToUnstructured(sampleResource)
+	if err != nil {
+		return err
+	}
+
+	manifestClient, err := r.getManifestClient(logger)
+	if err != nil {
+		sampleResource.Status.State = v1alpha1.SampleStateError
+		return r.Client.Status().Update(ctx, sampleResource)
+	}
+
+	// Use manifest library client to install a sample chart
+	readyToBeDeleted, err := manifestClient.Uninstall(manifestLib.InstallInfo{
+		Ctx: ctx,
+		ChartInfo: &manifestLib.ChartInfo{
+			ChartName:   "nginx-ingress",
+			URL:         "https://helm.nginx.com/stable",
+			RepoName:    "my-chart-repo",
+			ReleaseName: "sampleReleaseName",
+		},
+		RemoteInfo: custom.RemoteInfo{
+			// destination cluster rest config
+			RemoteConfig: r.Config,
+			// destination cluster rest client
+			RemoteClient: &r.Client,
+		},
+		ResourceInfo: manifestLib.ResourceInfo{
+			// base operator resource to be passed for custom checks
+			BaseResource: &unstructured.Unstructured{Object: sampleObjUnstructured},
+		},
+		CheckFn: func(context.Context, *unstructured.Unstructured, *logr.Logger, custom.RemoteInfo) (bool, error) {
+			// your custom logic here to check is all resources were removed
+			return true, nil
+		},
+		CheckReadyStates: false,
+	})
+
+	if err != nil {
+		return err
+	}
+	if readyToBeDeleted {
+		// Example: If Deleting state, remove Finalizers
+		sampleResource.Finalizers = nil
+		return r.Client.Update(ctx, sampleResource)
+	}
+	return nil
 }
 
 func (r *SampleReconciler) HandleErrorState(ctx context.Context, sampleResource *v1alpha1.Sample) error {
@@ -118,6 +233,7 @@ func (r *SampleReconciler) HandleReadyState(_ context.Context, _ *v1alpha1.Sampl
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *SampleReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	r.Config = mgr.GetConfig()
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.Sample{}).
 		Complete(r)
