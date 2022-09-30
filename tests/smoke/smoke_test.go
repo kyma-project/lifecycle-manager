e0f0e9418990e6b0076a2c8b59c9efb401c76bea
D067928 <jakob.moeller@sap.com>
2022-09-30 14:04:08 +0200

    Cleanup Smoke Tests

--- tests/smoke/smoke_test.go
+++ tests/smoke/smoke_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kyma-project/lifecycle-manager/tests/smoke/internal"
+	"github.com/kyma-project/lifecycle-manager/operator/api/v1alpha1"
 	"gopkg.in/yaml.v3"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -18,20 +18,14 @@ import (
 	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
-	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
 
 var (
 	TestEnv env.Environment
+	ControllerDeploymentName = "lifecycle-manager-controller-manager"
+	KCP = "kcp-system"
 )
-
-var provisionType *string
-
-func init() {
-	provisionType = flag.String("provision-type", "kyma-cli",
-		"Defines the Provisioning Type")
-}
 
 func TestMain(m *testing.M) {
 	log.Println("setting up test environment from flags")
@@ -41,103 +35,138 @@ func TestMain(m *testing.M) {
 	}
 
 	flag.Parse()
-
 	log.Println("creating test environment")
+
 	TestEnv = env.NewWithConfig(cfg)
 
-	switch *provisionType {
-	case "kyma-cli":
-		setupKymaProvisioning("kyma", TestEnv)
-	case "kind":
-		setupKindProvisioning("kind", TestEnv)
-	}
-
 	os.Exit(TestEnv.Run(m))
-}
-
-func setupKindProvisioning(cluster string, testEnv env.Environment) {
-	log.Println("registering setup hooks")
-	testEnv.Setup(
-		envfuncs.CreateKindCluster(cluster),
-		internal.InstallWithKustomize(
-			// you could use "github.com/kyma-project/lifecycle-manager//operator/config/default"
-			"../../operator/config/default",
-		),
-		func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
-			labels := cfg.Labels()
-			if labels == nil {
-				labels = make(map[string]string)
-			}
-			labels["test-type.kyma-project.io"] = "smoke"
-			cfg.WithLabels(labels)
-			return ctx, nil
-		},
-	)
-	log.Println("registering finish hooks")
-	testEnv.Finish(envfuncs.DestroyKindCluster(cluster))
-}
-
-func setupKymaProvisioning(cluster string, testEnv env.Environment) {
-	log.Println("registering setup hooks")
-	testEnv.Setup(
-		internal.CreateKymaK3dCluster(cluster),
-		internal.InstallWithKustomize(
-			// you could use "github.com/kyma-project/lifecycle-manager//operator/config/default"
-			"../../operator/config/default",
-		),
-	)
-	log.Println("registering finish hooks")
-	testEnv.Finish(internal.DestroyKymaK3dCluster(cluster))
 }
 
 func TestControllerManagerSpinsUp(t *testing.T) {
 	depFeature := features.New("appsv1/deployment/controller-manager").
 		WithLabel("app.kubernetes.io/component", "lifecycle-manager.kyma-project.io").
 		WithLabel("test-type.kyma-project.io", "smoke").
-		Assess("exists", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			client, err := cfg.NewClient()
-			if err != nil {
-				t.Fatal(err)
-			}
-			dep := ControllerManagerDeployment("kcp-system", "lifecycle-manager-controller-manager")
-			// wait for the deployment to finish becoming available
-			err = wait.For(conditions.New(
-				client.Resources()).ResourcesFound(&appsv1.DeploymentList{Items: []appsv1.Deployment{dep}}),
-			)
-			if err != nil {
-				t.Fatal(err)
-			}
-			return ctx
-		}).
-		Assess("available", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			client, err := cfg.NewClient()
-			if err != nil {
-				t.Fatal(err)
-			}
-			dep := ControllerManagerDeployment("kcp-system", "lifecycle-manager-controller-manager")
-			// wait for the deployment to finish becoming available
-			err = wait.For(conditions.New(client.Resources()).DeploymentConditionMatch(dep.DeepCopy(),
-				appsv1.DeploymentAvailable, corev1.ConditionTrue),
-				wait.WithTimeout(time.Minute*3))
-
-			pods := corev1.PodList{}
-			_ = client.Resources("kcp-system").List(ctx, &pods)
-			for _, pod := range pods.Items {
-				if marshal, err := yaml.Marshal(&pod.Status); err == nil {
-					t.Logf("Pod Status For %s/%s\n%s", pod.Namespace, pod.Name, marshal)
-				}
-			}
-
-			logDeployStatus(t, ctx, client, dep)
-
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			return ctx
-		}).Feature()
+		Assess("exists", deploymentExists(KCP, ControllerDeploymentName)).
+		Assess("available", deploymentAvailable(KCP, ControllerDeploymentName)).
+		Assess("kyma creation", kymaCreate(KCP, "default-kyma")).
+		Assess("kyma readiness", kymaReady(KCP, "default-kyma")).
+		Feature()
 
 	TestEnv.Test(t, depFeature)
+}
+
+func kymaReady(namespace string, name string) features.Func {
+	return func(ctx context.Context, t *testing.T, config *envconf.Config) context.Context {
+		client := config.Client()
+
+		var kyma v1alpha1.Kyma
+		if err := wait.For(func() (done bool, err error) {
+			if err := client.Resources().Get(ctx, name, namespace, &kyma); err != nil {
+				t.Fatal(err)
+			}
+			return kyma.Status.State == v1alpha1.StateReady, nil
+		}); err != nil {
+			t.Fatal(err)
+		}
+		logKymaStatus(t, ctx, client, kyma)
+
+		return ctx
+	}
+}
+
+func kymaCreate(namespace, name string) features.Func {
+	return func(ctx context.Context, t *testing.T, config *envconf.Config) context.Context {
+		client := config.Client()
+		kyma := NewTestKyma(namespace, name)
+
+		if err := client.Resources().Create(ctx, kyma); err != nil {
+			t.Fatal(err)
+		}
+
+		if err := wait.For(conditions.New(
+			client.Resources()).ResourcesFound(&v1alpha1.KymaList{Items: []v1alpha1.Kyma{*kyma}}),
+		); err != nil {
+			t.Fatal(err)
+		}
+
+		return ctx
+	}
+}
+
+func deploymentAvailable(namespace, name string) features.Func {
+	return func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+		client, err := cfg.NewClient()
+		if err != nil {
+			t.Fatal(err)
+		}
+		dep := ControllerManagerDeployment(namespace, name)
+		// wait for the deployment to finish becoming available
+		err = wait.For(
+			conditions.New(client.Resources()).DeploymentConditionMatch(
+				dep.DeepCopy(),
+				appsv1.DeploymentAvailable, corev1.ConditionTrue,
+			),
+			wait.WithTimeout(time.Minute*3),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+
+		pods := corev1.PodList{}
+		_ = client.Resources(namespace).List(ctx, &pods, func(options *metav1.ListOptions) {
+			sel, err := metav1.LabelSelectorAsSelector(dep.Spec.Selector)
+			if err != nil {
+				t.Fatal(err)
+			}
+			options.LabelSelector = sel.String()
+		})
+
+		for _, pod := range pods.Items {
+			if marshal, err := yaml.Marshal(&pod.Status); err == nil {
+				t.Logf("Pod Status For %s/%s\n%s", pod.Namespace, pod.Name, marshal)
+			}
+		}
+
+		logDeployStatus(t, ctx, client, dep)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		return ctx
+	}
+}
+
+func deploymentExists(namespace, name string) features.Func {
+	return func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+		client, err := cfg.NewClient()
+		if err != nil {
+			t.Fatal(err)
+		}
+		dep := ControllerManagerDeployment(namespace, name)
+		// wait for the deployment to finish becoming available
+		err = wait.For(
+			conditions.New(
+				client.Resources(),
+			).ResourcesFound(&appsv1.DeploymentList{Items: []appsv1.Deployment{dep}}),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return ctx
+	}
+}
+
+func logKymaStatus(t *testing.T, ctx context.Context, client klient.Client, kyma v1alpha1.Kyma) {
+	errCheckCtx, cancelErrCheck := context.WithTimeout(ctx, 5*time.Second)
+	defer cancelErrCheck()
+	if err := client.Resources().Get(errCheckCtx, kyma.Name, kyma.Namespace, &kyma); err != nil {
+		t.Error(err)
+	}
+	if marshal, err := yaml.Marshal(&kyma.Status); err == nil {
+		t.Logf("%s", marshal)
+	}
 }
 
 func logDeployStatus(t *testing.T, ctx context.Context, client klient.Client, dep appsv1.Deployment) {
@@ -155,5 +184,22 @@ func ControllerManagerDeployment(namespace string, name string) appsv1.Deploymen
 	return appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace,
 			Labels: map[string]string{"app.kubernetes.io/component": "lifecycle-manager.kyma-project.io"}},
+	}
+}
+
+func NewTestKyma(namespace, name string) *v1alpha1.Kyma {
+	return &v1alpha1.Kyma{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: v1alpha1.GroupVersion.String(),
+			Kind:       string(v1alpha1.KymaKind),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      envconf.RandomName(name, 8),
+			Namespace: namespace,
+		},
+		Spec: v1alpha1.KymaSpec{
+			Modules: []v1alpha1.Module{},
+			Channel: v1alpha1.DefaultChannel,
+		},
 	}
 }
