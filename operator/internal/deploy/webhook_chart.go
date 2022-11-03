7428d7a37009412f9f56de739f628026f2edc0ca
khlifi411 <46890720+khlifi411@users.noreply.github.com>
2022-11-03 14:43:29 +0100

    remove unused method

--- operator/internal/deploy/webhook_chart.go
+++ operator/internal/deploy/webhook_chart.go
@@ -5,14 +5,9 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/kyma-project/lifecycle-manager/operator/pkg/remote"
-	appsv1 "k8s.io/api/apps/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/kyma-project/lifecycle-manager/operator/api/v1alpha1"
 	moduleLib "github.com/kyma-project/module-manager/operator/pkg/manifest"
 	moduleLibTypes "github.com/kyma-project/module-manager/operator/pkg/types"
 )
@@ -40,21 +35,6 @@ var (
 type WatchableConfig struct {
 	Labels     map[string]string `json:"labels"`
 	StatusOnly bool              `json:"statusOnly"`
-}
-
-func (m *SKRWebhookChartManager) IsSkrChartRemoved(ctx context.Context, kyma *v1alpha1.Kyma,
-	remoteClientCache *remote.ClientCache, kcpClient client.Client,
-) bool {
-	skrClient, err := remote.NewRemoteClient(ctx, kcpClient, client.ObjectKeyFromObject(kyma),
-		kyma.Spec.Sync.Strategy, remoteClientCache)
-	if err != nil {
-		return false
-	}
-	err = skrClient.Get(ctx, client.ObjectKey{
-		Namespace: metav1.NamespaceDefault,
-		Name:      ResolveSKRChartResourceName(DeploymentNameTpl),
-	}, &appsv1.Deployment{})
-	return apierrors.IsNotFound(err)
 }
 
 func ResolveSKRChartResourceName(resourceNameTpl string) string {
