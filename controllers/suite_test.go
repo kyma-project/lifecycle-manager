ccd7004cbbcaee769ee0b08f2e7de04d6961e563
Jeremy Harisch <jeremy.harisch@sap.com>
2022-12-08 15:04:52 +0100

    Fix chart paths

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -84,7 +84,7 @@ var _ = BeforeSuite(func() {
 	// manifest CRD
 	// istio CRDs
 	remoteCrds, err := ParseRemoteCRDs([]string{
-		"https://raw.githubusercontent.com/kyma-project/module-manager/main/operator/config/crd/bases/operator.kyma-project.io_manifests.yaml", //nolint:lll
+		"https://raw.githubusercontent.com/kyma-project/module-manager/main/config/crd/bases/operator.kyma-project.io_manifests.yaml", //nolint:lll
 	})
 	Expect(err).NotTo(HaveOccurred())
 
