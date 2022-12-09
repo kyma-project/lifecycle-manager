e88e52d50f5f4dc6d9c8c591ebea930d68cdac4f
Xin Ruan <xin.ruan@sap.com>
2022-12-09 15:26:35 +0100

    and new test for random channel and refactoring channel version upgrade test

--- controllers/kyma_module_channel_test.go
+++ controllers/kyma_module_channel_test.go
@@ -13,7 +13,32 @@ import (
 	. "github.com/kyma-project/lifecycle-manager/pkg/testutils"
 )
 
-var _ = Describe("Switching of a Channel leading to an Upgrade", Ordered, func() {
+const (
+	FastChannel   = "fast"
+	RandomChannel = "random"
+	LowerVersion  = "0.0.1"
+	HigherVersion = "0.0.2"
+)
+
+var _ = Describe("A random channel should be deployed successful", func() {
+	kyma := NewTestKyma("kyma")
+	kyma.Spec.Modules = append(
+		kyma.Spec.Modules, v1alpha1.Module{
+			ControllerName: "manifest",
+			Name:           "module-with-random-channel",
+			Channel:        RandomChannel,
+		})
+
+	BeforeEach(func() {
+		SetupModuleTemplateSetsForKyma(kyma, LowerVersion, RandomChannel)
+	})
+	It("expect moduleStatus in random channel", func() {
+		Expect(controlPlaneClient.Create(ctx, kyma)).ToNot(HaveOccurred())
+		Eventually(expectEveryModuleStatusToHaveChannel(kyma.Name, RandomChannel), Timeout, Interval).Should(Succeed())
+	})
+})
+
+var _ = Describe("Switching of a Channel with higher version leading to an Upgrade", Ordered, func() {
 	kyma := NewTestKyma("empty-module-kyma")
 
 	kyma.Spec.Modules = append(
@@ -27,11 +52,15 @@ var _ = Describe("Switching of a Channel leading to an Upgrade", Ordered, func()
 		Expect(controlPlaneClient.Delete(ctx, kyma)).Should(Succeed())
 	})
 
-	BeforeAll(SetupModuleTemplateSetsForKyma(kyma))
+	BeforeAll(func() {
+		SetupModuleTemplateSetsForKyma(kyma, LowerVersion, v1alpha1.DefaultChannel)
+		SetupModuleTemplateSetsForKyma(kyma, HigherVersion, FastChannel)
+	})
+
 	AfterAll(CleanupModuleTemplateSetsForKyma(kyma))
 
 	It(
-		"should create kyma with standard modules in regular normally", func() {
+		"should create kyma with standard modules in default channel normally", func() {
 			Expect(controlPlaneClient.Create(ctx, kyma)).ToNot(HaveOccurred())
 			Eventually(GetKymaState(kyma.Name), 5*time.Second, Interval).
 				Should(BeEquivalentTo(string(v1alpha1.StateProcessing)))
@@ -51,19 +80,22 @@ var _ = Describe("Switching of a Channel leading to an Upgrade", Ordered, func()
 			Eventually(expectedBehavior, Timeout, Interval).Should(Succeed())
 		},
 		Entry(
-			"When kyma is deployed in default channel, expect ModuleStatus to be in regular channel",
+			"When kyma is deployed in default channel with lower version,"+
+				" expect ModuleStatus to be in regular channel",
 			noCondition(),
 			expectEveryModuleStatusToHaveChannel(kyma.Name, v1alpha1.DefaultChannel),
 		),
 		Entry(
-			"When all modules are updated to fast channel, expect ModuleStatus to update to fast channel",
-			whenUpdatingEveryModuleChannel(kyma.Name, "fast"),
-			expectEveryModuleStatusToHaveChannel(kyma.Name, "fast"),
+			"When all modules are updated to fast channel with higher version,"+
+				" expect ModuleStatus to update to fast channel",
+			whenUpdatingEveryModuleChannel(kyma.Name, FastChannel),
+			expectEveryModuleStatusToHaveChannel(kyma.Name, FastChannel),
 		),
 		Entry(
-			"When all modules are reverted to regular channel, expect ModuleStatus to stay in fast channel",
-			whenUpdatingEveryModuleChannel(kyma.Name, "regular"),
-			expectEveryModuleStatusToHaveChannel(kyma.Name, "fast"),
+			"When all modules are reverted to regular channel,"+
+				" expect ModuleStatus to stay in fast channel",
+			whenUpdatingEveryModuleChannel(kyma.Name, v1alpha1.DefaultChannel),
+			expectEveryModuleStatusToHaveChannel(kyma.Name, FastChannel),
 		),
 	)
 
@@ -77,34 +109,22 @@ var _ = Describe("Switching of a Channel leading to an Upgrade", Ordered, func()
 },
 )
 
-func SetupModuleTemplateSetsForKyma(kyma *v1alpha1.Kyma) func() {
-	return func() {
-		By("creating decremented ModuleTemplate set in regular")
-		for _, module := range kyma.Spec.Modules {
-			template, err := ModuleTemplateFactory(module, unstructured.Unstructured{})
-			By("decrementing the module template from the factory in the patch of the semantic version")
-			Expect(
-				template.Spec.ModifyDescriptor(
-					v1alpha1.ModifyDescriptorVersion(
-						func(version *semver.Version) string {
-							return fmt.Sprintf("v%v.%v.%v", version.Major(), version.Minor(), version.Patch()-1)
-						},
-					),
+func SetupModuleTemplateSetsForKyma(kyma *v1alpha1.Kyma, modifiedVersion, channel string) {
+	for _, module := range kyma.Spec.Modules {
+		template, err := ModuleTemplateFactory(module, unstructured.Unstructured{})
+		Expect(
+			template.Spec.ModifyDescriptor(
+				v1alpha1.ModifyDescriptorVersion(
+					func(version *semver.Version) string {
+						return modifiedVersion
+					},
 				),
-			).ToNot(HaveOccurred())
-
-			Expect(err).ShouldNot(HaveOccurred())
-			Expect(controlPlaneClient.Create(ctx, template)).To(Succeed())
-		}
-
-		By("Creating standard ModuleTemplate set in fast")
-		for _, module := range kyma.Spec.Modules {
-			template, err := ModuleTemplateFactory(module, unstructured.Unstructured{})
-			Expect(err).ShouldNot(HaveOccurred())
-			template.Spec.Channel = "fast"
-			template.Name = fmt.Sprintf("%s-%s", template.Name, "fast")
-			Expect(controlPlaneClient.Create(ctx, template)).To(Succeed())
-		}
+			),
+		).ToNot(HaveOccurred())
+		Expect(err).ShouldNot(HaveOccurred())
+		template.Spec.Channel = channel
+		template.Name = fmt.Sprintf("%s-%s", template.Name, channel)
+		Expect(controlPlaneClient.Create(ctx, template)).To(Succeed())
 	}
 }
 
@@ -113,13 +133,14 @@ func CleanupModuleTemplateSetsForKyma(kyma *v1alpha1.Kyma) func() {
 		By("Cleaning up decremented ModuleTemplate set in regular")
 		for _, module := range kyma.Spec.Modules {
 			template, err := ModuleTemplateFactory(module, unstructured.Unstructured{})
+			template.Name = fmt.Sprintf("%s-%s", template.Name, v1alpha1.DefaultChannel)
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(controlPlaneClient.Delete(ctx, template)).To(Succeed())
 		}
 		By("Cleaning up standard ModuleTemplate set in fast")
 		for _, module := range kyma.Spec.Modules {
 			template, err := ModuleTemplateFactory(module, unstructured.Unstructured{})
-			template.Name = fmt.Sprintf("%s-%s", template.Name, "fast")
+			template.Name = fmt.Sprintf("%s-%s", template.Name, FastChannel)
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(controlPlaneClient.Delete(ctx, template)).To(Succeed())
 		}
