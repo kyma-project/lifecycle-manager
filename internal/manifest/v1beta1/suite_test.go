c861ff79befc313b791f0a5716850721b34ba17c
Xin Ruan <xin.ruan@sap.com>
2023-03-16 14:16:26 +0100

    Improve private oci registry document (#471)
    * Improve Provide credential for private OCI registry authentication document
    Fix suite test to use correct kcp client
    
    * use different k8sclient to install external resources

--- internal/manifest/v1beta1/suite_test.go
+++ internal/manifest/v1beta1/suite_test.go
@@ -139,9 +139,8 @@ var _ = BeforeSuite(
 		)
 		Expect(err).NotTo(HaveOccurred())
 
-		k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-		Expect(err).NotTo(HaveOccurred())
-		Expect(k8sClient).NotTo(BeNil())
+		k8sClient = k8sManager.GetClient()
+
 		kcp := &declarative.ClusterInfo{Config: cfg, Client: k8sClient}
 		reconciler = declarative.NewFromManager(
 			k8sManager, &v1beta1.Manifest{},
