2cb246f0fce35a138791d2630b4e8cc34260b89c
jeremyharisch <jeremy@harisch.de>
2023-05-05 13:12:14 +0200

    Change conversion webhook

--- api/v1beta1/kyma_conversion.go
+++ api/v1beta1/kyma_conversion.go
@@ -8,10 +8,10 @@ import (
 func (src *Kyma) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*v1beta2.Kyma)
 	dst.ObjectMeta = src.ObjectMeta
+	if dst.ObjectMeta.Labels == nil {
+		dst.ObjectMeta.Labels = make(map[string]string)
+	}
 	if !src.Spec.Sync.Enabled {
-		if dst.ObjectMeta.Labels == nil {
-			dst.ObjectMeta.Labels = make(map[string]string)
-		}
 		dst.ObjectMeta.Labels[v1beta2.SyncLabel] = v1beta2.DisableLabelValue
 	} else {
 		dst.ObjectMeta.Labels[v1beta2.SyncLabel] = v1beta2.EnableLabelValue
