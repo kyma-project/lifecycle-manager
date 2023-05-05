0710545fd2c465abf2cc2f395bb55558190fc241
Xin Ruan <xin.ruan@sap.com>
2023-05-05 14:37:08 +0200

    bring back v1alpha1

---
+++ api/v1alpha1/watcher_types.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+//+kubebuilder:object:root=true
+//+kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:deprecatedversion:warning="kyma-project.io/v1alpha1 Watcher is deprecated. Use v1beta1 instead."
+//+kubebuilder:unservedversion
+
+// Watcher is the Schema for the watchers API.
+type Watcher struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// +kubebuilder:validation:Optional
+	metav1.ObjectMeta `json:"metadata"`
+
+	// +kubebuilder:validation:Optional
+	Spec v1beta2.WatcherSpec `json:"spec"`
+
+	Status v1beta2.WatcherStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// WatcherList contains a list of Watcher.
+type WatcherList struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// +kubebuilder:validation:Optional
+	metav1.ListMeta `json:"metadata"`
+	Items           []Watcher `json:"items"`
+}
+
+func init() { //nolint:gochecknoinits
+	SchemeBuilder.Register(&Watcher{}, &WatcherList{})
+}
