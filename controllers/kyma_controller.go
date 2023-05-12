11bcbec992683245d629ea37da6478d8d3a3e22c
Badr, Nesma <nesma.badr@sap.com>
2023-05-12 10:34:53 +0200

    fix linting

--- controllers/kyma_controller.go
+++ controllers/kyma_controller.go
@@ -77,6 +77,7 @@ type KymaReconciler struct {
 }
 
 //nolint:lll
+//nolint:funlen
 //+kubebuilder:rbac:groups=operator.kyma-project.io,resources=kymas,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=operator.kyma-project.io,resources=kymas/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=operator.kyma-project.io,resources=kymas/finalizers,verbs=update
