482a4b06815336c19273af032d3d656aa1050c2c
Badr, Nesma <nesma.badr@sap.com>
2023-05-25 13:36:18 +0200

    fix test

--- controllers/control-plane/kcp_controller_remote_sync_test.go
+++ controllers/control-plane/kcp_controller_remote_sync_test.go
@@ -209,7 +209,7 @@ var _ = Describe("Kyma sync into Remote Cluster", Ordered, func() {
 
 		By("Remote Kyma should contain Watcher labels and annotations")
 		Eventually(watcherLabelsAnnotationsExist, Timeout, Interval).
-			WithArguments(runtimeClient, remoteKyma, controllers.DefaultRemoteSyncNamespace).
+			WithArguments(runtimeClient, remoteKyma, kyma, controllers.DefaultRemoteSyncNamespace).
 			Should(Succeed())
 	})
 
