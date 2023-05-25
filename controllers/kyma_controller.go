9ca7f78163ee9e2a946648138c67dd22d5ce2dc5
Xin Ruan <xin.ruan@sap.com>
2023-05-25 21:10:43 +0200

    test RequeueAfter

--- controllers/kyma_controller.go
+++ controllers/kyma_controller.go
@@ -283,7 +283,7 @@ func (r *KymaReconciler) processKymaState(ctx context.Context, kyma *v1beta2.Kym
 	case v1beta2.StateError:
 		return ctrl.Result{Requeue: true}, r.handleProcessingState(ctx, kyma)
 	case v1beta2.StateReady:
-		return ctrl.Result{RequeueAfter: r.RequeueIntervals.Success}, r.handleProcessingState(ctx, kyma)
+		return ctrl.Result{Requeue: true, RequeueAfter: r.RequeueIntervals.Success}, r.handleProcessingState(ctx, kyma)
 	}
 
 	return ctrl.Result{}, nil
