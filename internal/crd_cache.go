14ce1e12b19354d9570ee45c0a0c271e1723398e
Badr, Nesma <nesma.badr@sap.com>
2023-06-01 16:37:19 +0200

    crd cache implementation

---
+++ internal/crd_cache.go
@@ -0,0 +1,32 @@
+package internal
+
+import (
+	"sync"
+
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+)
+
+type CustomResourceDefinitionCache struct {
+	storage *sync.Map
+}
+
+func NewCustomResourceDefinitionCache() *CustomResourceDefinitionCache {
+	return &CustomResourceDefinitionCache{storage: &sync.Map{}}
+}
+
+func (cache *CustomResourceDefinitionCache) Get(key string) *v1.CustomResourceDefinition {
+	value, ok := cache.storage.Load(key)
+	if !ok {
+		return nil
+	}
+
+	return value.(*v1.CustomResourceDefinition)
+}
+
+func (cache *CustomResourceDefinitionCache) Set(key string, value *v1.CustomResourceDefinition) {
+	cache.storage.Store(key, value)
+}
+
+func (cache *CustomResourceDefinitionCache) Delete(key string) {
+	cache.storage.Delete(key)
+}
