0069a8cb0f71285051c477f7fdbef1e11b6889c4
Badr, Nesma <nesma.badr@sap.com>
2023-06-19 16:23:52 +0200

    add comment

--- controllers/control-plane/kcp_controller_helper_test.go
+++ controllers/control-plane/kcp_controller_helper_test.go
@@ -214,6 +214,7 @@ func updateKymaCRD(clnt client.Client) (*v1extensions.CustomResourceDefinition, 
 	crd, err = fetchCrd(clnt, v1beta2.KymaKind)
 	kymaCrdName := fmt.Sprintf("%s.%s", v1beta2.KymaKind.Plural(), v1beta2.GroupVersion.Group)
 
+	// Replacing the CRD after updating the KCP CRD in order to valiate the generation numbers get updated correctly.
 	if _, ok := cache.GetCachedCRD(kymaCrdName); ok {
 		cache.SetCRDInCache(kymaCrdName, *crd)
 	}
