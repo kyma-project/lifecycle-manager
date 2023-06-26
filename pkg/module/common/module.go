a7d06c32f7aaed5b3a071e669e9b668a0a4a6082
Badr, Nesma <nesma.badr@sap.com>
2023-06-26 13:27:15 +0200

    fixes

--- pkg/module/common/module.go
+++ pkg/module/common/module.go
@@ -62,7 +62,7 @@ func (m *Module) ApplyLabelsAndAnnotations(
 
 func (m *Module) IsRemoteModuleTemplate(kyma *v1beta2.Kyma) bool {
 	for _, module := range kyma.Spec.Modules {
-		if module.Name == m.Template.Name {
+		if module.Name == m.ModuleName {
 			if module.RemoteModuleTemplateRef == "" {
 				return false
 			} else {
