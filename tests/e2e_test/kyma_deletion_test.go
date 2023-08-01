a1352fc52819062810695325a4f40ccdc53fe1d1
Badr, Nesma <nesma.badr@sap.com>
2023-08-01 17:58:34 +0200

    add test

---
+++ tests/e2e_test/kyma_deletion_test.go
@@ -0,0 +1,91 @@
+//go:build deletion_e2e
+
+package e2e_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+
+	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
+	"github.com/kyma-project/lifecycle-manager/pkg/testutils"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	timeout      = 10 * time.Second
+	readyTimeout = 2 * time.Minute
+	interval     = 1 * time.Second
+
+	watcherPodContainer = "server"
+	exampleSKRDomain    = "example.domain.com"
+
+	KLMPodPrefix    = "klm-controller-manager"
+	KLMPodContainer = "manager"
+
+	defaultRuntimeNamespace = "kyma-system"
+	defaultRemoteKymaName   = "default"
+
+	controlPlaneNamespace = "kcp-system"
+	localHostname         = "0.0.0.0"
+	k3dHostname           = "host.k3d.internal"
+)
+
+var (
+	errPodNotFound               = errors.New("could not find pod")
+	errWatcherDeploymentNotReady = errors.New("watcher Deployment is not ready")
+	errModuleNotExisting         = errors.New("module does not exists in KymaCR")
+	errLogNotFound               = errors.New("logMsg was not found in log")
+	errKymaNotInExpectedState    = errors.New("kyma CR not in expected state")
+	errKymaNotDeleted            = errors.New("kyma CR not deleted")
+)
+
+var _ = Describe("KCP Kyma CR should be deleted successfully when SKR cluster gets deleted",
+	Ordered, func() {
+		channel := "regular"
+		switchedChannel := "fast"
+		kyma := testutils.NewKymaForE2E("kyma-sample", "kcp-system", channel)
+		GinkgoWriter.Printf("kyma before create %v\n", kyma)
+		remoteNamespace := "kyma-system"
+
+		BeforeAll(func() {
+			//make sure we can list Kymas to ensure CRDs have been installed
+			err := controlPlaneClient.List(ctx, &v1beta2.KymaList{})
+			Expect(meta.IsNoMatchError(err)).To(BeFalse())
+		})
+
+		It("Should create empty Kyma CR on remote cluster", func() {
+			Eventually(controlPlaneClient.Create, timeout, interval).
+				WithContext(ctx).
+				WithArguments(kyma).
+				Should(Succeed())
+			By("verifying kyma is in Error state")
+			Eventually(checkKymaIsInState, readyTimeout, interval).
+				WithContext(ctx).
+				WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient, v1beta2.StateError).
+				Should(Succeed())
+		})
+	})
+
+func checkKymaIsInState(ctx context.Context,
+	kymaName, kymaNamespace string,
+	k8sClient client.Client,
+	expectedState v1beta2.State,
+) error {
+	kyma := &v1beta2.Kyma{}
+	if err := k8sClient.Get(ctx, client.ObjectKey{Name: kymaName, Namespace: kymaNamespace}, kyma); err != nil {
+		return err
+	}
+	GinkgoWriter.Printf("kyma %v\n", kyma)
+	if kyma.Status.State != expectedState {
+		return fmt.Errorf("%w: expect %s, but in %s",
+			errKymaNotInExpectedState, expectedState, kyma.Status.State)
+	}
+	return nil
+}
