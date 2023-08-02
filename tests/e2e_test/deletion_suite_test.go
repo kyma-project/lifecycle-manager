fdc108e35a84787b332e4ddf5ff1ac882311b685
Badr, Nesma <nesma.badr@sap.com>
2023-08-02 14:56:18 +0200

    test fixes

---
+++ tests/e2e_test/deletion_suite_test.go
@@ -0,0 +1,59 @@
+//go:build deletion_e2e
+
+package e2e_test
+
+import (
+	"errors"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+
+	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
+	"github.com/kyma-project/lifecycle-manager/pkg/testutils"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var (
+	errKymaNotDeleted = errors.New("kyma CR not deleted")
+)
+
+const (
+	timeout       = 10 * time.Second
+	statusTimeout = 2 * time.Minute
+	interval      = 1 * time.Second
+)
+
+var _ = Describe("KCP Kyma CR should be deleted successfully when SKR cluster gets deleted",
+	Ordered, func() {
+		channel := "regular"
+		kyma := testutils.NewKymaForE2E("kyma-sample", "kcp-system", channel)
+		GinkgoWriter.Printf("kyma before create %v\n", kyma)
+
+		BeforeAll(func() {
+			//make sure we can list Kymas to ensure CRDs have been installed
+			err := controlPlaneClient.List(ctx, &v1beta2.KymaList{})
+			Expect(meta.IsNoMatchError(err)).To(BeFalse())
+		})
+
+		It("Should create empty Kyma CR on remote cluster", func() {
+			Eventually(CreateKymaSecret, timeout, interval).
+				WithContext(ctx).
+				WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient).
+				Should(Succeed())
+			Eventually(controlPlaneClient.Create, timeout, interval).
+				WithContext(ctx).
+				WithArguments(kyma).
+				Should(Succeed())
+			By("verifying kyma is ready")
+			Eventually(CheckKymaIsInState, readyTimeout, interval).
+				WithContext(ctx).
+				WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient, v1beta2.StateReady).
+				Should(Succeed())
+			By("verifying remote kyma is ready")
+			Eventually(CheckRemoteKymaCR, readyTimeout, interval).
+				WithContext(ctx).
+				WithArguments(remoteNamespace, []v1beta2.Module{}, runtimeClient, v1beta2.StateReady).
+				Should(Succeed())
+		})
+	})
