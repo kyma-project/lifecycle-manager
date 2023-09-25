3a79a5e0658275923f7f8821d390eb22af292562
Hukumraj Singh Deora <me@hukumraj-singh-deora.party>
2023-09-25 11:33:51 +0200

    kyma_deletion_test.go.go: Delete Metrics Before Removing Finalizer

--- tests/e2e_test/kyma_deletion_test.go
+++ tests/e2e_test/kyma_deletion_test.go
@@ -3,7 +3,11 @@
 package e2e_test
 
 import (
+	"io"
+	"net/http"
 	"os/exec"
+	"regexp"
+	"strconv"
 	"time"
 
 	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
@@ -119,3 +123,93 @@ var _ = Describe("KCP Kyma CR should be deleted successfully when SKR cluster ge
 				Should(Succeed())
 		})
 	})
+
+var _ = Describe("When KCP Kyma CR deleted, Kyma Status Metric should also be deleted",
+	Ordered, func() {
+		channel := "regular"
+		kyma := testutils.NewKymaForE2E("kyma-sample", "kcp-system", channel)
+		remoteNamespace := "kyma-system"
+		GinkgoWriter.Printf("kyma before create %v\n", kyma)
+
+		BeforeAll(func() {
+			// make sure we can list Kymas to ensure CRDs have been installed
+			err := controlPlaneClient.List(ctx, &v1beta2.KymaList{})
+			Expect(meta.IsNoMatchError(err)).To(BeFalse())
+		})
+
+		It("Should create empty Kyma CR", func() {
+			Eventually(CreateKymaSecret, timeout, interval).
+				WithContext(ctx).
+				WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient).
+				Should(Succeed())
+			Eventually(controlPlaneClient.Create, timeout, interval).
+				WithContext(ctx).
+				WithArguments(kyma).
+				Should(Succeed())
+			By("verifying kyma is ready")
+			Eventually(CheckKymaIsInState, readyTimeout, interval).
+				WithContext(ctx).
+				WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient, v1beta2.StateReady).
+				Should(Succeed())
+			By("verifying remote kyma is ready")
+			Eventually(CheckRemoteKymaCR, readyTimeout, interval).
+				WithContext(ctx).
+				WithArguments(remoteNamespace, []v1beta2.Module{}, runtimeClient, v1beta2.StateReady).
+				Should(Succeed())
+		})
+
+		It("Kyma reconciliation should remove metric when Kyma CR deleted ", func() {
+			By("getting the current metric count")
+			metricCountBeforeDeletion, err := getKymaStateMetricCount()
+			Expect(err).Should(Not(HaveOccurred()))
+			GinkgoWriter.Printf("Kyma State Metric count before CR deletion: %d", metricCountBeforeDeletion)
+
+			By("deleting KCP Kyma")
+			Eventually(controlPlaneClient.Delete, readyTimeout, interval).
+				WithContext(ctx).
+				WithArguments(kyma).
+				Should(Succeed())
+
+			By("waiting for Kyma CR to be removed")
+			Eventually(CheckKCPKymaCRDeleted, 2*time.Minute, interval).
+				WithContext(ctx).
+				WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient).
+				Should(Succeed())
+
+			By("should decrease the metric count")
+			metricCountAfterDeletion, err := getKymaStateMetricCount()
+			Expect(err).Should(Not(HaveOccurred()))
+			GinkgoWriter.Printf("Kyma State Metric count after CR deletion: %d", metricCountAfterDeletion)
+			Expect(metricCountAfterDeletion).To(Equal(metricCountBeforeDeletion - 1))
+		})
+	})
+
+func getKymaStateMetricCount() (int, error) {
+	response, err := http.Get("http://localhost:9081/metrics")
+	if err != nil {
+		return 0, err
+	}
+	defer response.Body.Close()
+	bodyBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return 0, err
+	}
+	bodyString := string(bodyBytes)
+
+	sum := 0
+	states := []string{"Deleting", "Error", "Processing", "Ready", "Warning"}
+	for _, state := range states {
+		re := regexp.MustCompile(
+			`lifecycle_mgr_kyma_state{instance_id="[^"]+",kyma_name="[^"]+",shoot="[^"]+",state="` +
+				state + `"} (\d+)`)
+		match := re.FindStringSubmatch(bodyString)
+		if len(match) > 1 {
+			count, err := strconv.Atoi(match[1])
+			if err != nil {
+				return 0, err
+			}
+			sum += count
+		}
+	}
+	return sum, nil
+}
