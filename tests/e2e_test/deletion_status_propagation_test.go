3598a6f77f36b4a1e4251b7caa352f58234abfce
Hukumraj Singh Deora <me@hukumraj-singh-deora.party>
2023-10-16 16:45:52 +0200

    e2e_test: Fix Lint Issues

--- tests/e2e_test/deletion_status_propagation_test.go
+++ tests/e2e_test/deletion_status_propagation_test.go
@@ -5,14 +5,18 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 
 	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
-	"github.com/kyma-project/lifecycle-manager/pkg/testutils"
+	. "github.com/kyma-project/lifecycle-manager/pkg/testutils"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Warning Status Propagation After When Deletion Timestamp Not Zero", Ordered, func() {
-	kyma := testutils.NewKymaForE2E("kyma-sample", "kcp-system", "regular")
-	GinkgoWriter.Printf("kyma before create %v\n", kyma)
+	kyma := NewKymaWithSyncLabel("kyma-sample", "kcp-system", "regular",
+		v1beta2.SyncStrategyLocalSecret)
+	moduleName := "template-operator"
+
+	InitEmptyKymaBeforeAll(kyma)
+	CleanupKymaAfterAll(kyma)
 
 	BeforeAll(func() {
 		// make sure we can list Kymas to ensure CRDs have been installed
@@ -20,48 +24,29 @@ var _ = Describe("Warning Status Propagation After When Deletion Timestamp Not Z
 		Expect(meta.IsNoMatchError(err)).To(BeFalse())
 	})
 
-	It("Should create empty Kyma CR on remote cluster", func() {
-		Eventually(CreateKymaSecret).
-			WithContext(ctx).
-			WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient).
-			Should(Succeed())
-		Eventually(controlPlaneClient.Create).
-			WithContext(ctx).
-			WithArguments(kyma).
-			Should(Succeed())
-		By("verifying kyma is ready")
-		Eventually(CheckKymaIsInState).
-			WithContext(ctx).
-			WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient, v1beta2.StateReady).
-			Should(Succeed())
-		By("verifying remote kyma is ready")
-		Eventually(CheckRemoteKymaCR).
-			WithContext(ctx).
-			WithArguments(remoteNamespace, []v1beta2.Module{}, runtimeClient, v1beta2.StateReady).
-			Should(Succeed())
-	})
-
-	It("Should enable Template Operator and Kyma should result in Warning status", func() {
-		By("Enabling Template Operator")
-		Eventually(EnableModule).
-			WithContext(ctx).
-			WithArguments(defaultRemoteKymaName, remoteNamespace, "template-operator", "regular", runtimeClient).
-			Should(Succeed())
-		By("Checking state of kyma")
-		Eventually(CheckKymaIsInState).
-			WithContext(ctx).
-			WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient, v1beta2.StateWarning).
-			Should(Succeed())
+	Context("Given Template Operator", func() {
+		It("When enabling Template Operator", func() {
+			Eventually(EnableModule).
+				WithContext(ctx).
+				WithArguments(defaultRemoteKymaName, remoteNamespace, moduleName, "regular", runtimeClient).
+				Should(Succeed())
+		})
+		It("Kyma should be in Warning state", func() {
+			Eventually(CheckKymaIsInState).
+				WithContext(ctx).
+				WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient, v1beta2.StateWarning).
+				Should(Succeed())
+		})
 	})
 
 	It("Should propagate Warning state even after resource is marked for deletion", func() {
 		By("Getting manifest object key")
 		manifestObjectKey, err := GetManifestObjectKey(ctx, controlPlaneClient,
-			kyma.GetName(), kyma.GetNamespace(), "template-operator")
+			kyma.GetName(), kyma.GetNamespace(), moduleName)
 		Expect(err).To(Not(HaveOccurred()))
 		By("Getting resource object key")
 		resourceObjectKey, err := GetResourceObjectKey(ctx, controlPlaneClient,
-			kyma.GetName(), kyma.GetNamespace(), "template-operator")
+			kyma.GetName(), kyma.GetNamespace(), moduleName)
 		Expect(err).To(Not(HaveOccurred()))
 		By("Adding finalizer to avoid deletion")
 		Eventually(AddFinalizerToSampleResource).
@@ -72,7 +57,7 @@ var _ = Describe("Warning Status Propagation After When Deletion Timestamp Not Z
 		By("Disabling Template Operator")
 		Eventually(DisableModule).
 			WithContext(ctx).
-			WithArguments(defaultRemoteKymaName, remoteNamespace, "template-operator", runtimeClient).
+			WithArguments(defaultRemoteKymaName, remoteNamespace, moduleName, runtimeClient).
 			Should(Succeed())
 
 		By("Checking manifest deletion timestamp")
@@ -95,20 +80,5 @@ var _ = Describe("Warning Status Propagation After When Deletion Timestamp Not Z
 		By("Remove finalizer")
 		err = RemoveFinalizerToSampleResource(ctx, *resourceObjectKey, runtimeClient, "e2e-finalizer")
 		Expect(err).To(Not(HaveOccurred()))
-	})
-
-	It("Should delete KCP Kyma", func() {
-		By("Deleting KCP Kyma")
-		Eventually(controlPlaneClient.Delete).
-			WithContext(ctx).
-			WithArguments(kyma).
-			Should(Succeed())
-	})
-
-	It("Kyma CR should be removed", func() {
-		Eventually(CheckKCPKymaCRDeleted).
-			WithContext(ctx).
-			WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient).
-			Should(Succeed())
 	})
 })
