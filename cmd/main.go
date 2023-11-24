49114baa28dca875f6efe9047c8eee28f6ba67ae
Xin Ruan <xin.ruan@sap.com>
2023-11-24 15:55:52 +0100

    refactor: Move metrics to internal package (#1084)
    move metrics to internal pkg

--- cmd/main.go
+++ cmd/main.go
@@ -51,8 +51,7 @@ import (
 	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
 	"github.com/kyma-project/lifecycle-manager/internal"
 	"github.com/kyma-project/lifecycle-manager/internal/controller"
-	kymametrics "github.com/kyma-project/lifecycle-manager/internal/controller/kyma/metrics"   //nolint:importas // ad-hoc import collision resolution
-	purgemetrics "github.com/kyma-project/lifecycle-manager/internal/controller/purge/metrics" //nolint:importas // ad-hoc import collision resolution
+	"github.com/kyma-project/lifecycle-manager/internal/pkg/metrics"
 	"github.com/kyma-project/lifecycle-manager/pkg/log"
 	"github.com/kyma-project/lifecycle-manager/pkg/matcher"
 	"github.com/kyma-project/lifecycle-manager/pkg/queue"
@@ -63,7 +62,7 @@ import (
 	_ "github.com/open-component-model/ocm/pkg/contexts/ocm"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	//nolint:gci // kubebuilder's scaffold imports must be appended here.
-	//+kubebuilder:scaffold:imports
+	// +kubebuilder:scaffold:imports
 )
 
 var (
@@ -83,7 +82,7 @@ func init() {
 	machineryutilruntime.Must(istioclientapiv1beta1.AddToScheme(scheme))
 
 	machineryutilruntime.Must(v1beta2.AddToScheme(scheme))
-	//+kubebuilder:scaffold:scheme
+	// +kubebuilder:scaffold:scheme
 }
 
 func main() {
@@ -161,7 +160,7 @@ func setupManager(flagVar *FlagVar, newCacheOptions cache.Options, scheme *machi
 		enableWebhooks(mgr)
 	}
 
-	//+kubebuilder:scaffold:builder
+	// +kubebuilder:scaffold:builder
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
@@ -265,7 +264,7 @@ func setupKymaReconciler(mgr ctrl.Manager,
 		os.Exit(1)
 	}
 
-	kymametrics.Initialize()
+	metrics.InitKymaMetrics()
 }
 
 func createSkrWebhookManager(mgr ctrl.Manager, flagVar *FlagVar) (watcher.SKRWebhookManager, error) {
@@ -309,7 +308,7 @@ func setupPurgeReconciler(mgr ctrl.Manager,
 		setupLog.Error(err, "unable to create controller", "controller", "PurgeReconciler")
 		os.Exit(1)
 	}
-	purgemetrics.Initialize()
+	metrics.InitPurgeMetrics()
 }
 
 func setupManifestReconciler(
@@ -355,11 +354,13 @@ func dropVersionFromStoredVersions(mgr manager.Manager, versionToBeRemoved strin
 	cfg := mgr.GetConfig()
 	kcpClient, err := clientset.NewForConfig(cfg)
 	if err != nil {
-		setupLog.V(log.DebugLevel).Error(err, fmt.Sprintf("unable to initialize client to remove %s", versionToBeRemoved))
+		setupLog.V(log.DebugLevel).Error(err,
+			fmt.Sprintf("unable to initialize client to remove %s", versionToBeRemoved))
 	}
 	ctx := context.TODO()
 	var crdList *apiextensionsv1.CustomResourceDefinitionList
-	if crdList, err = kcpClient.ApiextensionsV1().CustomResourceDefinitions().List(ctx, apimetav1.ListOptions{}); err != nil {
+	if crdList, err = kcpClient.ApiextensionsV1().CustomResourceDefinitions().List(ctx,
+		apimetav1.ListOptions{}); err != nil {
 		setupLog.V(log.InfoLevel).Error(err, "unable to list CRDs")
 	}
 
