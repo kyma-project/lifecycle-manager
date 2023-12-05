2ddacb7c276d6b4569d5fd146ddbb1f9e18592b7
Xin Ruan <xin.ruan@sap.com>
2023-12-05 18:46:46 +0100

    only update manifest spec under deleting

--- pkg/module/sync/runner.go
+++ pkg/module/sync/runner.go
@@ -122,8 +122,8 @@ func (r *Runner) doUpdateWithStrategy(ctx context.Context, owner string, isEnabl
 	if isEnabledModule {
 		return r.patchManifest(ctx, owner, manifestObj)
 	}
-	// For disabled module, the manifest CR is under deleting, in this case, we only update when it's still not deleted.
-	if err := r.updateAvailableManifest(ctx, manifestObj); err != nil && !util.IsNotFound(err) {
+	// For disabled module, the manifest CR is under deleting, in this case, we only update the spec when it's still not deleted.
+	if err := r.updateAvailableManifestSpec(ctx, manifestObj); err != nil && !util.IsNotFound(err) {
 		return err
 	}
 	return nil
@@ -140,15 +140,15 @@ func (r *Runner) patchManifest(ctx context.Context, owner string, manifestObj *v
 	return nil
 }
 
-func (r *Runner) updateAvailableManifest(ctx context.Context, manifestObj *v1beta2.Manifest) error {
+func (r *Runner) updateAvailableManifestSpec(ctx context.Context, manifestObj *v1beta2.Manifest) error {
 	manifestInCluster := &v1beta2.Manifest{}
 
 	if err := r.Get(ctx, client.ObjectKey{Namespace: manifestObj.GetNamespace(), Name: manifestObj.GetName()},
 		manifestInCluster); err != nil {
 		return fmt.Errorf("error get manifest %s: %w", client.ObjectKeyFromObject(manifestObj), err)
 	}
-	manifestObj.ResourceVersion = manifestInCluster.ResourceVersion
-	if err := r.Update(ctx, manifestObj); err != nil {
+	manifestInCluster.Spec = manifestObj.Spec
+	if err := r.Update(ctx, manifestInCluster); err != nil {
 		return fmt.Errorf("error update manifest %s: %w", client.ObjectKeyFromObject(manifestObj), err)
 	}
 	return nil
