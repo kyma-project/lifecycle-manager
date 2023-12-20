6af8bf7abbf6407e443c7e3da245724abaa566dc
Badr, Nesma <nesma.badr@sap.com>
2023-12-20 11:27:06 +0100

    Have one test for deletion

--- tests/e2e/kyma_deletion_test.go
+++ tests/e2e/kyma_deletion_test.go
@@ -14,7 +14,7 @@ import (
 	. "github.com/kyma-project/lifecycle-manager/pkg/testutils"
 )
 
-var _ = Describe("Foreground Kyma CR Deletion", Ordered, func() {
+var _ = Describe("KCP Kyma CR Deletion After SKR Cluster Removal", Ordered, func() {
 	kyma := NewKymaWithSyncLabel("kyma-sample", "kcp-system", v1beta2.DefaultChannel,
 		v1beta2.SyncStrategyLocalSecret)
 	module := NewTemplateOperator(v1beta2.DefaultChannel)
