54ef77ce66f778330af53d3c2e96e1ecd423be5c
Xin Ruan <xin.ruan@sap.com>
2023-12-27 07:44:42 +0100

    fix: Bump cli-runtime to 0.29.0 (#1193)
    * chore(dependabot): bump k8s.io/cli-runtime from 0.28.4 to 0.29.0
    
    Bumps [k8s.io/cli-runtime](https://github.com/kubernetes/cli-runtime) from 0.28.4 to 0.29.0.
    - [Commits](https://github.com/kubernetes/cli-runtime/compare/v0.28.4...v0.29.0)
    
    ---
    updated-dependencies:
    - dependency-name: k8s.io/cli-runtime
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * upgrade k8s.io api to 0.29.0
    
    * clear up helm factory implementation
    
    * cleanup RESTClient
    
    * upgrade /pkg/authn/kubernetes
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- internal/declarative/v2/kube_factory_proxy.go
+++ internal/declarative/v2/kube_factory_proxy.go
@@ -5,13 +5,9 @@ import (
 
 	apicorev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
-	apimetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/resource"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/kubectl/pkg/util/openapi"
-	"k8s.io/kubectl/pkg/validation"
 )
 
 // OpenAPISchema returns metadata and structural information about
@@ -49,8 +45,7 @@ func (s *SingletonClients) UnstructuredClientForMapping(mapping *meta.RESTMappin
 	cfg.ContentConfig = resource.UnstructuredPlusDefaultContentConfig()
 	cfg.GroupVersion = &gv
 
-	var err error
-	client, err = rest.RESTClientForConfigAndClient(cfg, s.httpClient)
+	client, err := rest.RESTClientForConfigAndClient(cfg, s.httpClient)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create httpClient config: %w", err)
 	}
@@ -81,56 +76,11 @@ func (s *SingletonClients) ClientForMapping(mapping *meta.RESTMapping) (resource
 	gv := gvk.GroupVersion()
 	cfg.GroupVersion = &gv
 
-	var err error
-	client, err = rest.RESTClientForConfigAndClient(cfg, s.httpClient)
+	client, err := rest.RESTClientForConfigAndClient(cfg, s.httpClient)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create httpClient config: %w", err)
 	}
 
 	s.structuredRESTClientCache[key] = client
 	return client, nil
-}
-
-// KubernetesClientSet gives you back an external clientset.
-func (s *SingletonClients) KubernetesClientSet() (*kubernetes.Clientset, error) {
-	return s.kubernetesClient, nil
-}
-
-// DynamicClient returns a dynamic client ready for use.
-func (s *SingletonClients) DynamicClient() (*dynamic.DynamicClient, error) {
-	return s.dynamicClient, nil
-}
-
-// NewBuilder returns a new resource builder for structured api objects.
-func (s *SingletonClients) NewBuilder() *resource.Builder {
-	return resource.NewBuilder(s)
-}
-
-// RESTClient returns a RESTClient for accessing Kubernetes resources or an error.
-func (s *SingletonClients) RESTClient() (*rest.RESTClient, error) {
-	restClient, err := rest.RESTClientForConfigAndClient(s.config, s.httpClient)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize RestCliient for k8s api access: %w", err)
-	}
-	return restClient, nil
-}
-
-// Validator returns a schema that can validate objects stored on disk.
-func (s *SingletonClients) Validator(
-	validationDirective string,
-) (validation.Schema, error) {
-	if validationDirective == apimetav1.FieldValidationIgnore {
-		return validation.NullSchema{}, nil
-	}
-
-	resources, err := s.OpenAPISchema()
-	if err != nil {
-		return nil, err
-	}
-
-	conjSchema := validation.ConjunctiveSchema{
-		validation.NewSchemaValidation(resources),
-		validation.NoDoubleKeySchema{},
-	}
-	return validation.NewParamVerifyingSchema(conjSchema, nil, validationDirective), nil
 }
