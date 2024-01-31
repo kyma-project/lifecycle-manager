d1afdb0d75a0bd10640571da1d1ca55c2d149b68
Badr, Nesma <nesma.badr@sap.com>
2024-01-31 16:41:06 +0100

    Add script for storage version migrator

---
+++ scripts/storage_version_migrator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+while getopts c: flag
+
+do
+        case "${flag}" in
+                c) context=${OPTARG};;
+                *) echo "Invalid option: -$flag" ;;
+        esac
+done
+
+git clone https://github.com/kubernetes-sigs/kube-storage-version-migrator.git
+cd kube-storage-version-migrator
+kubectl config use-context $context
+make local-manifests REGISTRY=eu.gcr.io/k8s-artifacts-prod/storage-migrator VERSION=v0.0.5 NAMESPACE=kcp-system
+pushd manifests.local
+kubectl apply -k ./
+popd
+
