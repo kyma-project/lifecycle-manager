2c039488133a2875ffd722a72a2cc4d1490ec99e
Badr, Nesma <nesma.badr@sap.com>
2024-02-01 09:14:32 +0100

    Remove namespace from script

--- scripts/storage_version_migrator.sh
+++ scripts/storage_version_migrator.sh
@@ -11,7 +11,7 @@ done
 git clone https://github.com/kubernetes-sigs/kube-storage-version-migrator.git
 cd kube-storage-version-migrator
 kubectl config use-context $context
-make local-manifests REGISTRY=eu.gcr.io/k8s-artifacts-prod/storage-migrator VERSION=v0.0.5 NAMESPACE=kcp-system
+make local-manifests REGISTRY=eu.gcr.io/k8s-artifacts-prod/storage-migrator VERSION=v0.0.5
 pushd manifests.local
 kubectl apply -k ./
 popd
