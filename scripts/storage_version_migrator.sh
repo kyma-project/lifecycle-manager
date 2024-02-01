82d5057bc339988af8e6fbf8c68eea2ef58ed979
Badr, Nesma <nesma.badr@sap.com>
2024-02-01 13:43:35 +0100

    change namespacegit

--- scripts/storage_version_migrator.sh
+++ scripts/storage_version_migrator.sh
@@ -11,12 +11,12 @@ done
 git clone https://github.com/kubernetes-sigs/kube-storage-version-migrator.git
 cd kube-storage-version-migrator
 kubectl config use-context $context
-make local-manifests REGISTRY=eu.gcr.io/k8s-artifacts-prod/storage-migrator VERSION=v0.0.5 NAMESPACE=kcp-system
+make local-manifests REGISTRY=eu.gcr.io/k8s-artifacts-prod/storage-migrator VERSION=v0.0.5
 pushd manifests.local
 kubectl apply -k ./
 popd
 
-# This script deploys the storage version migrator. The migrator deploys its resources in the `kcp-system` namespace
+# This script deploys the storage version migrator. The migrator deploys its resources in the `kube-system` namespace
 # and it gets triggered every 10 minutes to migrate all resources stored in the etcd to the latest storage version.
 
 # To run the script, use the following command:
