63d3497ce3a85cd14f072f863dcae94b615fae44
Badr, Nesma <nesma.badr@sap.com>
2024-02-01 13:40:00 +0100

    change namespace

--- scripts/storage_version_migrator.sh
+++ scripts/storage_version_migrator.sh
@@ -11,8 +11,16 @@ done
 git clone https://github.com/kubernetes-sigs/kube-storage-version-migrator.git
 cd kube-storage-version-migrator
 kubectl config use-context $context
-make local-manifests REGISTRY=eu.gcr.io/k8s-artifacts-prod/storage-migrator VERSION=v0.0.5
+make local-manifests REGISTRY=eu.gcr.io/k8s-artifacts-prod/storage-migrator VERSION=v0.0.5 NAMESPACE=kcp-system
 pushd manifests.local
 kubectl apply -k ./
 popd
 
+# This script deploys the storage version migrator. The migrator deploys its resources in the `kcp-system` namespace
+# and it gets triggered every 10 minutes to migrate all resources stored in the etcd to the latest storage version.
+
+# To run the script, use the following command:
+
+# `sh ./scripts/storage_version_migrator.sh -c ${CONTEXT}`
+
+#where CONTEXT is the context for the cluster where you want to do the API migration
