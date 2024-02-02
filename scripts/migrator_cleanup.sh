b0cdaabbae7378579f489b9e47c28bf8e738293d
Badr, Nesma <nesma.badr@sap.com>
2024-02-02 10:37:07 +0100

    Add cleanup script

---
+++ scripts/migrator_cleanup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+while getopts c: flag
+
+do
+        case "${flag}" in
+                c) context=${OPTARG};;
+                *) echo "Invalid option: -$flag" ;;
+        esac
+done
+
+kubectl config use-context $context
+
+
+kubectl delete clusterrolebinding storage-version-migration-migrator
+kubectl delete clusterrolebinding storage-version-migration-trigger
+kubectl delete clusterrolebinding storage-version-migration-crd-creator
+kubectl delete clusterrolebinding storage-version-migration-initializer
+
+kubectl delete clusterrole storage-version-migration-crd-creator
+kubectl delete clusterrole storage-version-migration-initializer
+kubectl delete clusterrole storage-version-migration-trigger
+
+kubectl delete deployment migrator -n kube-system
+kubectl delete deployment trigger -n kube-system
+
+kubectl delete crd storageversionmigrations.migration.k8s.io
+kubectl delete crd storagestates.migration.k8s.io
+
+
