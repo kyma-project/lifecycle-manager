761f64cf6a8cc42873b1195d9c30a78b42d794bf
Benjamin Lindner <benjamin.lindner@sap.com>
2024-02-07 09:10:47 +0100

    add nil check and tmpl error. increase coverage

--- internal/descriptor/provider/provider_test.go
+++ internal/descriptor/provider/provider_test.go
@@ -13,13 +13,62 @@ import (
 	"github.com/kyma-project/lifecycle-manager/pkg/testutils/builder"
 )
 
+func TestGetDescriptor_OnEmptySpec_ReturnsErrDecode(t *testing.T) {
+	descriptorProvider := provider.NewCachedDescriptorProvider(nil) // assuming it handles nil cache internally
+	template := &v1beta2.ModuleTemplate{}
+
+	_, err := descriptorProvider.GetDescriptor(template)
+
+	require.Error(t, err)
+	assert.ErrorIs(t, err, provider.ErrDecode)
+}
+
+func TestAdd_OnNilTemplate_ReturnsErrTemplateNil(t *testing.T) {
+	descriptorProvider := provider.NewCachedDescriptorProvider(nil)
+
+	err := descriptorProvider.Add(nil)
+
+	require.Error(t, err)
+	assert.ErrorIs(t, err, provider.ErrTemplateNil)
+}
+
+func TestGetDescriptor_OnNilTemplate_ReturnsErrTemplateNil(t *testing.T) {
+	descriptorProvider := provider.NewCachedDescriptorProvider(nil)
+
+	_, err := descriptorProvider.GetDescriptor(nil)
+
+	require.Error(t, err)
+	assert.ErrorIs(t, err, provider.ErrTemplateNil)
+}
+
+func TestGetDescriptor_DecodeError(t *testing.T) {
+	descriptorProvider := provider.NewCachedDescriptorProvider(nil)
+	template := builder.NewModuleTemplateBuilder().WithRawDescriptor([]byte{}).WithDescriptor(nil).Build()
+
+	_, err := descriptorProvider.GetDescriptor(template)
+
+	require.Error(t, err)
+	assert.ErrorIs(t, err, provider.ErrDescriptorNil)
+}
+
 func TestGetDescriptor_OnEmptyCache_ReturnsParsedDescriptor(t *testing.T) {
 	descriptorCache := cache.NewDescriptorCache()
 	descriptorProvider := provider.NewCachedDescriptorProvider(descriptorCache)
 	template := builder.NewModuleTemplateBuilder().Build()
+
 	_, err := descriptorProvider.GetDescriptor(template)
 
 	require.NoError(t, err)
+}
+
+func TestAdd_DecodeError(t *testing.T) {
+	descriptorProvider := provider.NewCachedDescriptorProvider(nil)
+	template := builder.NewModuleTemplateBuilder().WithRawDescriptor([]byte{}).WithDescriptor(nil).Build()
+
+	err := descriptorProvider.Add(template)
+
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), provider.ErrDecode.Error())
 }
 
 func TestGetDescriptor_OnEmptyCache_AddsDescriptorFromTemplate(t *testing.T) {
