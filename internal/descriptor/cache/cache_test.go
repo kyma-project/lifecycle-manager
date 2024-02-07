a7d50d4683028a242c3fd9e896845647883e883b
Benjamin Lindner <benjamin.lindner@sap.com>
2024-02-07 08:00:54 +0100

    add AAA visual separation

--- internal/descriptor/cache/cache_test.go
+++ internal/descriptor/cache/cache_test.go
@@ -12,16 +12,16 @@ import (
 )
 
 func TestGet_ForCacheWithoutEntry_ReturnsNoEntry(t *testing.T) {
-	sut := cache.NewDescriptorCache()
+	descriptorCache := cache.NewDescriptorCache()
 	key := cache.DescriptorKey("key 1")
 
-	actual := sut.Get(key)
+	actual := descriptorCache.Get(key)
 
 	assert.Nil(t, actual)
 }
 
 func TestGet_ForCacheWithAnEntry_ReturnsAnEntry(t *testing.T) {
-	sut := cache.NewDescriptorCache()
+	descriptorCache := cache.NewDescriptorCache()
 	key1 := cache.DescriptorKey("key 1")
 	ocmDesc1 := &compdesc.ComponentDescriptor{
 		ComponentSpec: compdesc.ComponentSpec{
@@ -31,40 +31,37 @@ func TestGet_ForCacheWithAnEntry_ReturnsAnEntry(t *testing.T) {
 		},
 	}
 	desc1 := &v1beta2.Descriptor{ComponentDescriptor: ocmDesc1}
-	sut.Set(key1, desc1)
-	assertDescriptorEqual(t, desc1, sut.Get(key1))
+
+	descriptorCache.Set(key1, desc1)
+
+	assertDescriptorEqual(t, desc1, descriptorCache.Get(key1))
 }
 
-func TestGet_ForCacheWithOverwrittenEntry_ReturnsCorrectEntry(t *testing.T) {
-	sut := cache.NewDescriptorCache()
-	key1 := cache.DescriptorKey("key 1")
-	ocmDesc1 := &compdesc.ComponentDescriptor{
-		ComponentSpec: compdesc.ComponentSpec{
-			ObjectMeta: ocmmetav1.ObjectMeta{
-				Name: "descriptor 1",
+func TestGet_ForCacheWithOverwrittenEntry_ReturnsNewEntry(t *testing.T) {
+	descriptorCache := cache.NewDescriptorCache()
+	originalKey, originalValue := cache.DescriptorKey("key 1"), &v1beta2.Descriptor{
+		ComponentDescriptor: &compdesc.ComponentDescriptor{
+			ComponentSpec: compdesc.ComponentSpec{
+				ObjectMeta: ocmmetav1.ObjectMeta{Name: "descriptor 1"},
 			},
 		},
 	}
-	desc1 := &v1beta2.Descriptor{ComponentDescriptor: ocmDesc1}
-	sut.Set(key1, desc1)
-
-	ocmDesc2 := &compdesc.ComponentDescriptor{
-		ComponentSpec: compdesc.ComponentSpec{
-			ObjectMeta: ocmmetav1.ObjectMeta{
-				Name: "descriptor 2",
+	newKey, newValue := cache.DescriptorKey("key 2"), &v1beta2.Descriptor{
+		ComponentDescriptor: &compdesc.ComponentDescriptor{
+			ComponentSpec: compdesc.ComponentSpec{
+				ObjectMeta: ocmmetav1.ObjectMeta{Name: "descriptor 2"},
 			},
 		},
 	}
-	desc2 := &v1beta2.Descriptor{ComponentDescriptor: ocmDesc2}
+	descriptorCache.Set(originalKey, originalValue)
+	assertDescriptorNotEqual(t, newValue, descriptorCache.Get(originalKey))
+	assert.Nil(t, descriptorCache.Get(newKey))
 
-	assertDescriptorNotEqual(t, desc2, sut.Get(key1))
-	key2 := cache.DescriptorKey("key 2")
-	assert.Nil(t, sut.Get(key2))
-	sut.Set(key2, desc2)
-	assertDescriptorEqual(t, desc2, sut.Get(key2))
+	descriptorCache.Set(newKey, newValue)
+	descriptorCache.Set(originalKey, newValue)
 
-	sut.Set(key1, desc2)
-	assertDescriptorEqual(t, desc2, sut.Get(key2))
+	assertDescriptorEqual(t, newValue, descriptorCache.Get(newKey))
+	assertDescriptorEqual(t, newValue, descriptorCache.Get(originalKey))
 }
 
 func assertDescriptorEqual(t *testing.T, expected, actual *v1beta2.Descriptor) {
