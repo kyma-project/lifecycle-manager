f0d35f3d2cbe75a3278e94e05a7c44f6dff7ab5d
Badr, Nesma <nesma.badr@sap.com>
2024-02-21 17:35:30 +0100

    Revert "Remove v1beta1 crds"
    This reverts commit e7f0b8e240ce9d57ca75555b0b2db2d85b7b09c8.

---
+++ api/v1beta1/watcher_types.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	apimetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
+)
+
+// +kubebuilder:object:root=true
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:subresource:status
+// +kubebuilder:deprecatedversion:warning="kyma-project.io/v1beta1 Watcher is deprecated. Use v1beta2 instead."
+// +kubebuilder:unservedversion
+
+// Watcher is the Schema for the watchers API.
+type Watcher struct {
+	apimetav1.TypeMeta `json:",inline"`
+
+	// +kubebuilder:validation:Optional
+	apimetav1.ObjectMeta `json:"metadata"`
+
+	// +kubebuilder:validation:Optional
+	Spec v1beta2.WatcherSpec `json:"spec"`
+
+	Status v1beta2.WatcherStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// WatcherList contains a list of Watcher.
+type WatcherList struct {
+	apimetav1.TypeMeta `json:",inline"`
+
+	// +kubebuilder:validation:Optional
+	apimetav1.ListMeta `json:"metadata"`
+	Items              []Watcher `json:"items"`
+}
+
+//nolint:gochecknoinits // registers Watcher CRD on startup
+func init() {
+	SchemeBuilder.Register(&Watcher{}, &WatcherList{})
+}
