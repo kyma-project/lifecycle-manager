74c1e413fc08e5c6e873ae575fe36eecc4517685
Benjamin Lindner <benjamin.lindner@sap.com>
2024-02-26 07:48:54 +0100

    format

--- internal/manifest/filemutex/cache_test.go
+++ internal/manifest/filemutex/cache_test.go
@@ -1,11 +1,11 @@
 package filemutex_test
 
 import (
-	"github.com/stretchr/testify/require"
 	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/kyma-project/lifecycle-manager/internal/manifest/filemutex"
 )
