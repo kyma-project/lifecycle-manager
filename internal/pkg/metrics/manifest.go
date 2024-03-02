6ea2a0394aca30108b9d172855803ad1375ebeb1
Hukumraj Singh Deora <me@hukumraj-singh-deora.party>
2024-03-02 11:58:13 +0100

    fix:Lint Issue

--- internal/pkg/metrics/manifest.go
+++ internal/pkg/metrics/manifest.go
@@ -47,9 +47,11 @@ func NewManifestMetrics(sharedMetrics *SharedMetrics) *ManifestMetrics {
 			prometheus.HistogramOpts{
 				Name: MetricReconcileDuration,
 				Help: "Histogram of reconcile duration for manifest reconciliation in seconds",
-				Buckets: []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6,
+				Buckets: []float64{
+					0.005, 0.01, 0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6,
 					0.7, 0.8, 0.9, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5, 6, 7, 8, 9, 10, 15, 20,
-					25, 30, 40, 50, 60},
+					25, 30, 40, 50, 60,
+				},
 			},
 			[]string{MetricLabelModule},
 		),
