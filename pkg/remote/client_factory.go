c61a8cddd1c47f4adb3b598bbde9a9205253b0e4
Benjamin Lindner <benjamin.lindner@sap.com>
2024-03-26 09:16:19 +0100

    change client type

--- pkg/remote/client_factory.go
+++ pkg/remote/client_factory.go
@@ -1,11 +1,9 @@
 package remote
 
 import (
-	"github.com/prometheus/prometheus/storage/remote"
-
 	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
 )
 
 type ClientFactory interface {
-	GetClient(kyma *v1beta2.Kyma, syncNamespace string) (remote.Client, error)
+	GetClient(kyma *v1beta2.Kyma, syncNamespace string) (Client, error)
 }
