00bc6d2ef912d998d19aa46e89a16aec57e63af0
Raj <54686422+LeelaChacha@users.noreply.github.com>
2024-06-25 23:29:52 +0200

    chore: Add helpful comment
    Co-authored-by: Christoph Schwägerl <acc.pius@mailbox.org>

--- pkg/module/sync/runner.go
+++ pkg/module/sync/runner.go
@@ -134,6 +134,7 @@ func (r *Runner) doUpdateWithStrategy(ctx context.Context, owner string, isEnabl
 	}
 
 	if !NeedToUpdate(manifestInCluster, manifestObj, kymaModuleStatus) {
+		// Point to the current state from the cluster for the outside sync of the manifest
 		*manifestObj = *manifestInCluster
 		return nil
 	}
