05f5fe35daf8c9411283e187f14a1762c088b24f
jeremyharisch <jeremy@harisch.de>
2024-06-27 10:20:06 +0200

    Fix go lint

--- tests/e2e/module_status_decoupling_test.go
+++ tests/e2e/module_status_decoupling_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	templatev1alpha1 "github.com/kyma-project/template-operator/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	"github.com/kyma-project/lifecycle-manager/api/shared"
 	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
@@ -30,37 +31,7 @@ var _ = Describe("Module Status Decoupling", Ordered, func() {
 				Should(Succeed())
 		})
 
-		It("Then Module CR exists", func() {
-			Eventually(ModuleCRExists).
-				WithContext(ctx).
-				WithArguments(runtimeClient, moduleCR).
-				Should(Succeed())
-
-			By("And resource is defined in Manifest CR")
-			Eventually(func(g Gomega, ctx context.Context) {
-				resource, err := GetManifestResource(ctx, controlPlaneClient,
-					kyma.GetName(), kyma.GetNamespace(), module.Name)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(resource.GetName()).To(Equal(moduleCR.GetName()))
-				Expect(resource.GetNamespace()).To(Equal(moduleCR.GetNamespace()))
-				Expect(resource.GroupVersionKind().Version).To(Equal(moduleCR.GroupVersionKind().Version))
-				Expect(resource.GroupVersionKind().Group).To(Equal(moduleCR.GroupVersionKind().Group))
-				Expect(resource.GroupVersionKind().Kind).To(Equal(moduleCR.GroupVersionKind().Kind))
-			}).WithContext(ctx).Should(Succeed())
-
-			By("And Module in KCP Kyma CR is in \"Ready\" State")
-			Eventually(CheckModuleState).
-				WithContext(ctx).
-				WithArguments(controlPlaneClient, kyma.GetName(), kyma.GetNamespace(),
-					module.Name, shared.StateReady).
-				Should(Succeed())
-
-			By("And KCP kyma CR is in \"Ready\" State")
-			Eventually(KymaIsInState).
-				WithContext(ctx).
-				WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient, shared.StateReady).
-				Should(Succeed())
-		})
+		checkModuleStatus(module, moduleCR, kyma, shared.StateReady)
 
 		It("When Kyma Module is disabled", func() {
 			Eventually(DisableModule).
@@ -134,37 +105,7 @@ var _ = Describe("Module Status Decoupling", Ordered, func() {
 				Should(Succeed())
 		})
 
-		It("Then Module CR exists", func() {
-			Eventually(ModuleCRExists).
-				WithContext(ctx).
-				WithArguments(runtimeClient, moduleCR).
-				Should(Succeed())
-
-			By("And resource is defined in Manifest CR")
-			Eventually(func(g Gomega, ctx context.Context) {
-				resource, err := GetManifestResource(ctx, controlPlaneClient,
-					kyma.GetName(), kyma.GetNamespace(), moduleWrongConfig.Name)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(resource.GetName()).To(Equal(moduleCR.GetName()))
-				Expect(resource.GetNamespace()).To(Equal(moduleCR.GetNamespace()))
-				Expect(resource.GroupVersionKind().Version).To(Equal(moduleCR.GroupVersionKind().Version))
-				Expect(resource.GroupVersionKind().Group).To(Equal(moduleCR.GroupVersionKind().Group))
-				Expect(resource.GroupVersionKind().Kind).To(Equal(moduleCR.GroupVersionKind().Kind))
-			}).WithContext(ctx).Should(Succeed())
-
-			By("And Module in KCP Kyma CR is in \"Error\" State")
-			Eventually(CheckModuleState).
-				WithContext(ctx).
-				WithArguments(controlPlaneClient, kyma.GetName(), kyma.GetNamespace(),
-					moduleWrongConfig.Name, shared.StateError).
-				Should(Succeed())
-
-			By("And KCP kyma CR is in \"Error\" State")
-			Eventually(KymaIsInState).
-				WithContext(ctx).
-				WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient, shared.StateError).
-				Should(Succeed())
-		})
+		checkModuleStatus(moduleWrongConfig, moduleCR, kyma, shared.StateError)
 
 		It("When Kyma Module is disabled", func() {
 			Eventually(DisableModule).
@@ -197,5 +138,40 @@ var _ = Describe("Module Status Decoupling", Ordered, func() {
 				Should(Succeed())
 		})
 	})
-
 })
+
+func checkModuleStatus(module v1beta2.Module, moduleCR *unstructured.Unstructured, kyma *v1beta2.Kyma,
+	expectedState shared.State,
+) {
+	It("Then Module CR exists", func() {
+		Eventually(ModuleCRExists).
+			WithContext(ctx).
+			WithArguments(runtimeClient, moduleCR).
+			Should(Succeed())
+
+		By("And resource is defined in Manifest CR")
+		Eventually(func(g Gomega, ctx context.Context) {
+			resource, err := GetManifestResource(ctx, controlPlaneClient,
+				kyma.GetName(), kyma.GetNamespace(), module.Name)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(resource.GetName()).To(Equal(moduleCR.GetName()))
+			Expect(resource.GetNamespace()).To(Equal(moduleCR.GetNamespace()))
+			Expect(resource.GroupVersionKind().Version).To(Equal(moduleCR.GroupVersionKind().Version))
+			Expect(resource.GroupVersionKind().Group).To(Equal(moduleCR.GroupVersionKind().Group))
+			Expect(resource.GroupVersionKind().Kind).To(Equal(moduleCR.GroupVersionKind().Kind))
+		}).WithContext(ctx).Should(Succeed())
+
+		By("And Module in KCP Kyma CR is in " + string(expectedState) + " State")
+		Eventually(CheckModuleState).
+			WithContext(ctx).
+			WithArguments(controlPlaneClient, kyma.GetName(), kyma.GetNamespace(),
+				module.Name, expectedState).
+			Should(Succeed())
+
+		By("And KCP kyma CR is in " + string(expectedState) + " State")
+		Eventually(KymaIsInState).
+			WithContext(ctx).
+			WithArguments(kyma.GetName(), kyma.GetNamespace(), controlPlaneClient, expectedState).
+			Should(Succeed())
+	})
+}
