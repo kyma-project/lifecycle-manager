9ad1b9e4e3ef04804c6c8dcf8166c9ffd7f80054
Xin Ruan <xin.ruan@sap.com>
2024-08-26 16:18:39 +0200

    fix: Replace RestrictedEnqueueRequestForOwner with EnqueueRequestForOwner (#1792)
    * debug RestrictedEnqueueRequestForOwner
    
    * debug RestrictedEnqueueRequestForOwner
    
    * debug RestrictedEnqueueRequestForOwner
    
    * replace with EnqueueRequestForOwner
    
    * delete RestrictedEnqueueRequestForOwner
    
    * add ResourceVersionChangedPredicate
    
    * replace with ResourceVersionChangedPredicate
    
    * only have ResourceVersionChangedPredicate for Manifest CR
    
    * only have ResourceVersionChangedPredicate for Manifest CR
    
    * add predicate for manifest controller setup
    
    * add predicate for manifest controller setup

--- internal/controller/mandatorymodule/setup.go
+++ internal/controller/mandatorymodule/setup.go
@@ -5,7 +5,6 @@ import (
 
 	apicorev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/builder"
 	ctrlruntime "sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
@@ -20,16 +19,14 @@ const (
 )
 
 func (r *InstallationReconciler) SetupWithManager(mgr ctrl.Manager, opts ctrlruntime.Options) error {
-	predicates := predicate.Or(predicate.GenerationChangedPredicate{}, predicate.LabelChangedPredicate{})
 	if err := ctrl.NewControllerManagedBy(mgr).
 		For(&v1beta2.Kyma{}).
 		Named(installationControllerName).
 		WithOptions(opts).
-		WithEventFilter(predicates).
+		WithEventFilter(predicate.Or(predicate.GenerationChangedPredicate{}, predicate.LabelChangedPredicate{})).
 		Watches(
 			&v1beta2.ModuleTemplate{},
 			handler.EnqueueRequestsFromMapFunc(watch.NewMandatoryTemplateChangeHandler(r).Watch()),
-			builder.WithPredicates(predicates),
 		).
 		Watches(&apicorev1.Secret{}, handler.Funcs{}).
 		Complete(r); err != nil {
@@ -43,7 +40,7 @@ func (r *DeletionReconciler) SetupWithManager(mgr ctrl.Manager, opts ctrlruntime
 		For(&v1beta2.ModuleTemplate{}).
 		Named(deletionControllerName).
 		WithOptions(opts).
-		WithEventFilter(predicate.GenerationChangedPredicate{}).
+		WithEventFilter(predicate.Or(predicate.GenerationChangedPredicate{}, predicate.LabelChangedPredicate{})).
 		Complete(r); err != nil {
 		return fmt.Errorf("failed to setup manager for mandatory module deletion controller: %w", err)
 	}
