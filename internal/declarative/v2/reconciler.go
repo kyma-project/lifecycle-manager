2db02ca2dd5c2ff75825b02c2c5d640fe27c1a3c
Christoph Schwägerl <c.schwaegerl@sap.com>
2024-09-06 13:41:13 +0200

    remove re-setting the state in setManifestState

--- internal/declarative/v2/reconciler.go
+++ internal/declarative/v2/reconciler.go
@@ -69,6 +69,8 @@ type Reconciler struct {
 	specResolver           SpecResolver
 }
 
+const waitingForResourcesMsg = "waiting for resources to become ready"
+
 type ConditionType string
 
 const (
@@ -405,26 +407,20 @@ func (r *Reconciler) checkManagerState(ctx context.Context, clnt Client, target 
 	return managerState, nil
 }
 
-func (r *Reconciler) setManifestState(manifest *v1beta2.Manifest, state shared.State) error {
+func (r *Reconciler) setManifestState(manifest *v1beta2.Manifest, newState shared.State) error {
 	status := manifest.GetStatus()
 
-	if state == shared.StateProcessing {
-		waitingMsg := "waiting for resources to become ready"
-		manifest.SetStatus(status.WithState(shared.StateProcessing).WithOperation(waitingMsg))
+	if newState == shared.StateProcessing {
+		manifest.SetStatus(status.WithState(shared.StateProcessing).WithOperation(waitingForResourcesMsg))
 		return ErrInstallationConditionRequiresUpdate
 	}
 
-	if !manifest.GetDeletionTimestamp().IsZero() {
-		state = shared.StateDeleting
-	}
-
 	installationCondition := newInstallationCondition(manifest)
-	if !meta.IsStatusConditionTrue(status.Conditions,
-		installationCondition.Type) || status.State != state {
+	if newState != status.State || !meta.IsStatusConditionTrue(status.Conditions, installationCondition.Type) {
 		installationCondition.Status = apimetav1.ConditionTrue
 		meta.SetStatusCondition(&status.Conditions, installationCondition)
-		manifest.SetStatus(status.WithState(state).
-			WithOperation(installationCondition.Message))
+
+		manifest.SetStatus(status.WithState(newState).WithOperation(installationCondition.Message))
 		return ErrInstallationConditionRequiresUpdate
 	}
 
