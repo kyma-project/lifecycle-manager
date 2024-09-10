341b24fd4fdb7a50feef208d54890ce651b1cfad
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2024-09-10 10:03:14 +0200

    feat: Remove kyma.spec.modules[].version from api (#1837)
    * remove kyma.spec.modules[n].version from api
    
    * Skip test
    
    * disable test
    
    * skip test
    
    * Skip test
    
    * Update api/v1beta2/kyma_types.go
    
    Co-authored-by: Christoph Schwägerl <acc.pius@mailbox.org>
    
    ---------
    
    Co-authored-by: Christoph Schwägerl <acc.pius@mailbox.org>

--- api/v1beta2/kyma_types.go
+++ api/v1beta2/kyma_types.go
@@ -71,7 +71,6 @@ type Module struct {
 
 	// Channel is the desired channel of the Module. If this changes or is set, it will be used to resolve a new
 	// ModuleTemplate based on the new resolved resources.
-	// The Version and Channel are mutually exclusive options.
 	// +kubebuilder:validation:Pattern:=^[a-z]+$
 	// +kubebuilder:validation:MaxLength:=32
 	// +kubebuilder:validation:MinLength:=3
@@ -81,8 +80,9 @@ type Module struct {
 	// ModuleTemplate based on this specific version.
 	// The Version and Channel are mutually exclusive options.
 	// The regular expression come from here: https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-	// +kubebuilder:validation:Pattern:=`^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`
-	Version string `json:"version,omitempty"`
+        // json:"-" to disable installation of specific versions until decided to roll this out
+        // see https://github.com/kyma-project/lifecycle-manager/issues/1847
+        Version string `json:"-"`
 
 	// RemoteModuleTemplateRef is deprecated and will no longer have any functionality.
 	// It will be removed in the upcoming API version.
--- tests/integration/controller/kyma/kyma_module_version_test.go
+++ tests/integration/controller/kyma/kyma_module_version_test.go
@@ -12,6 +12,7 @@ import (
 var _ = Describe("Given invalid module version which is rejected by CRD validation rules", func() {
 	DescribeTable(
 		"Test enable module", func(givenCondition func() error) {
+			Skip("Version attribute is disabled for now on the CRD level")
 			Eventually(givenCondition, Timeout, Interval).Should(Succeed())
 		},
 
