9dee97906b7bb24246d8dbf411c5d648620bfb41
Xin Ruan <xin.ruan@sap.com>
2024-09-11 10:14:22 +0200

    fix: Use github action create release (#1835)
    * refactor create release github action
    ---------
    
    Co-authored-by: Jeremy Harisch <48282931+jeremyharisch@users.noreply.github.com>
    Co-authored-by: Tomasz Smelcerz <tomasz.smelcerz@sap.com>

--- .github/scripts/release/draft_release.sh
+++ .github/scripts/release/draft_release.sh
@@ -6,11 +6,10 @@ set -E
 set -o pipefail
 
 RELEASE_TAG=$1
-CHANGELOG_FILE_NAME=$2
-CHANGELOG_FILE=$(cat "$CHANGELOG_FILE_NAME")
 
-GITHUB_URL=https://api.github.com/repos/$CODE_REPOSITORY
-GITHUB_AUTH_HEADER="Authorization: Bearer $GITHUB_TOKEN"
+GITHUB_URL=https://api.github.com/repos/${CODE_REPOSITORY}
+GITHUB_AUTH_HEADER="Authorization: Bearer ${GITHUB_TOKEN}"
+CHANGELOG_FILE=$(cat CHANGELOG.md)
 
 JSON_PAYLOAD=$(jq -n \
   --arg tag_name "$RELEASE_TAG" \
@@ -24,15 +23,14 @@ JSON_PAYLOAD=$(jq -n \
   }')
 
 CURL_RESPONSE=$(curl -L \
-  -s \
-  --fail-with-body \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "$GITHUB_AUTH_HEADER" \
+  -H "${GITHUB_AUTH_HEADER}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "$GITHUB_URL"/releases \
+  "${GITHUB_URL}"/releases \
   -d "$JSON_PAYLOAD")
 
 # return the id of the release draft
 echo "$CURL_RESPONSE" | jq -r ".id"
+
 
--- .github/scripts/release/publish_release.sh
+++ .github/scripts/release/publish_release.sh
@@ -5,20 +5,16 @@ set -o errexit
 set -E
 set -o pipefail
 
-RELEASE_ID=$1
+RELEASE_VERSION=$1
 
-GITHUB_URL=https://api.github.com/repos/$CODE_REPOSITORY
-GITHUB_AUTH_HEADER="Authorization: Bearer $GITHUB_TOKEN"
+GITHUB_URL=https://api.github.com/repos/${CODE_REPOSITORY}
+GITHUB_AUTH_HEADER="Authorization: Bearer ${GITHUB_TOKEN}"
 
 CURL_RESPONSE=$(curl -L \
-  -s \
-  --fail-with-body \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "$GITHUB_AUTH_HEADER" \
+  -H "${GITHUB_AUTH_HEADER}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "$GITHUB_URL"/releases/"$RELEASE_ID" \
+  "${GITHUB_URL}"/releases/"${RELEASE_VERSION}" \
   -d '{"draft":false}')
-
 echo "$CURL_RESPONSE"
-
