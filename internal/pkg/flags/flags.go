238ac2b8c318cb34b5ba17702890a523210689b4
Christoph Schwägerl <c.schwaegerl@sap.com>
2024-09-13 15:39:21 +0200

    default watcher image

--- internal/pkg/flags/flags.go
+++ internal/pkg/flags/flags.go
@@ -54,6 +54,7 @@ const (
 	DefaultKymaListenerAddress                                          = ":8082"
 	DefaultManifestListenerAddress                                      = ":8083"
 	DefaultPprofAddress                                                 = ":8084"
+	DefaultWatcherImageTag                                              = "1.1.4"
 	DefaultWatcherImageName                                             = "runtime-watcher"
 	DefaultWatcherImageRegistry                                         = "europe-docker.pkg.dev/kyma-project/prod"
 	DefaultWatcherResourcesPath                                         = "./skr-webhook"
@@ -221,7 +222,7 @@ func DefineFlagVar() *FlagVar {
 			"comma-separated list of API versions, where each API version is in the format 'kind:version'.")
 	flag.StringVar(&flagVar.WatcherImageName, "skr-watcher-image-name", DefaultWatcherImageName,
 		`Image name to be used for the SKR watcher image.`)
-	flag.StringVar(&flagVar.WatcherImageTag, "skr-watcher-image-tag", "",
+	flag.StringVar(&flagVar.WatcherImageTag, "skr-watcher-image-tag", DefaultWatcherImageTag,
 		`Image tag to be used for the SKR watcher image.`)
 	flag.StringVar(&flagVar.WatcherImageRegistry, "skr-watcher-image-registry", DefaultWatcherImageRegistry,
 		`Image registry to be used for the SKR watcher image.`)
