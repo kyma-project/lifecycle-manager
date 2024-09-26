360b832e6bb947f8be3d82076877339482be8621
Christoph Schwägerl <c.schwaegerl@sap.com>
2024-09-26 13:56:32 +0200

    chore: VSCode tasks for local test setup

---
+++ scripts/tests/create_test_clusters.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Change to the directory where the script is located
+cd "$(dirname "$0")"
+
+# create SKR cluster
+if k3d cluster list | grep -q "skr"; then
+  echo "Reusing existing SKR cluster..."
+  else
+  k3d cluster create skr \
+        -p 10080:80@loadbalancer \
+        -p 10443:443@loadbalancer \
+        --k3s-arg --tls-san="skr.cluster.local@server:*" \
+        --image rancher/k3s:v1.28.7-k3s1 \
+        --k3s-arg --disable="traefik@server:*" \
+        --k3s-arg --tls-san="host.k3d.internal@server:*"
+fi
+
+# create KCP cluster
+if k3d cluster list | grep -q "kcp"; then
+  echo "Reusing existing KCP cluster..."
+  else
+  k3d cluster create kcp \
+        -p 9443:443@loadbalancer \
+        -p 9080:80@loadbalancer \
+        -p 9081:8080@loadbalancer \
+        --registry-create k3d-myregistry.localhost:5111 \
+        --image rancher/k3s:v1.28.7-k3s1 \
+        --k3s-arg --disable="traefik@server:*" \
+        --k3s-arg --tls-san="host.k3d.internal@server:*"
+  
+  kubectl config use-context k3d-kcp
+
+  # install istio
+  istioctl install --set profile=demo -y
+
+  # install cert-manager
+  kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.yaml
+
+  ./add_skr_host_to_coredns.sh
+
+  # create kcp-system namespace
+  kubectl create namespace kcp-system
+
+  # label node
+  kubectl label nodes k3d-kcp-server-0 iam.gke.io/gke-metadata-server-enabled="true" pool-type=mgmt
+fi
+
+# export kubeconfigs
+k3d kubeconfig get skr > ~/.k3d/skr-local.yaml 
+k3d kubeconfig get kcp > ~/.k3d/kcp-local.yaml
---
+++ scripts/tests/deploy_kyma.sh
@@ -0,0 +1,22 @@
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kyma-sample
+  namespace: kcp-system
+  labels:
+    "operator.kyma-project.io/kyma-name": "kyma-sample"
+    "operator.kyma-project.io/managed-by": "lifecycle-manager"
+data:
+  config: $(k3d kubeconfig get skr | sed 's/0\.0\.0\.0/host.k3d.internal/' | base64 | tr -d '\n')
+---
+apiVersion: operator.kyma-project.io/v1beta2
+kind: Kyma
+metadata:
+  annotations:
+    skr-domain: "example.domain.com"
+  name: kyma-sample
+  namespace: kcp-system
+spec:
+  channel: regular
+EOF
