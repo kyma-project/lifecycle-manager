fca4c6467a1fe76db4e00dcb26a41fcc945ebc48
Badr, Nesma <nesma.badr@sap.com>
2024-09-30 15:05:48 +0200

    start implementation

---
+++ api/v1beta2/modulereleasemeta_types.go
@@ -0,0 +1,8 @@
+package v1beta2
+
+// ModuleReleaseMeta
+
+// +genclient
+// +kubebuilder:object:root=true
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:storageversion
