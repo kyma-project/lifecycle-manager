8a83d42a09796fe489de4fba3c714c287046ef41
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2024-10-03 23:56:13 +0200

    reorder

--- internal/controller/purge/handler.go
+++ internal/controller/purge/handler.go
@@ -12,13 +12,12 @@ import (
 )
 
 type reconcileHandler struct {
-	Get                            func(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error
 	handleKymaNotFoundError        func(logger logr.Logger, kyma *v1beta2.Kyma, err error) (ctrl.Result, error)
-	handleKymaNotMarkedForDeletion func(ctx context.Context, kyma *v1beta2.Kyma) (ctrl.Result, error)
 	handlePurgeNotDue              func(logger logr.Logger, kyma *v1beta2.Kyma, requeueAfter time.Duration) (ctrl.Result, error)
+	Get                            func(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error
+	handleKymaNotMarkedForDeletion func(ctx context.Context, kyma *v1beta2.Kyma) (ctrl.Result, error)
 	handleSkrNotFoundError         func(ctx context.Context, kyma *v1beta2.Kyma, err error) (ctrl.Result, error)
 	handlePurge                    func(ctx context.Context, kyma *v1beta2.Kyma, remoteClient client.Client, start time.Time) (ctrl.Result, error)
 	calculateRequeueAfterTime      func(kyma *v1beta2.Kyma) time.Duration
-
-	SkrContextFactory remote.SkrContextProvider
+	SkrContextFactory              remote.SkrContextProvider
 }
