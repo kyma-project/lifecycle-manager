32abc3e3df58bb2e18b49fc389d3689543bc59a2
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2024-10-03 21:55:19 +0200

    add handler

---
+++ internal/controller/purge/handler.go
@@ -0,0 +1,20 @@
+package purge
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type reconcileHandler struct {
+	handleKymaNotFoundError        func(logger logr.Logger, kyma *v1beta2.Kyma, err error) (ctrl.Result, error)
+	handleKymaNotMarkedForDeletion func(ctx context.Context, kyma *v1beta2.Kyma) (ctrl.Result, error)
+	handlePurgeNotDue              func(logger logr.Logger, kyma *v1beta2.Kyma, requeueAfter time.Duration) (ctrl.Result, error)
+	handleSkrNotFoundError         func(ctx context.Context, kyma *v1beta2.Kyma, err error) (ctrl.Result, error)
+	handlePurge                    func(ctx context.Context, kyma *v1beta2.Kyma, remoteClient client.Client, start time.Time) (ctrl.Result, error)
+	Get                            func(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error
+}
