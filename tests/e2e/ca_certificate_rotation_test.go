897dd0f871be06cadba432dd11f2dbcc2d41ad8b
Raj <me@hukumraj-singh-deora.party>
2024-11-04 10:27:39 +0100

    fix: ca rotation e2e test

--- tests/e2e/ca_certificate_rotation_test.go
+++ tests/e2e/ca_certificate_rotation_test.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
-	apimetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
@@ -34,17 +33,21 @@ var _ = Describe("CA Certificate Rotation", Ordered, func() {
 			Namespace: RemoteNamespace,
 		}
 		It("Then KCP TLS Certificate is removed", func() {
-			timeNow := &apimetav1.Time{Time: time.Now()}
-			expectedLogMessage := "CA Certificate was rotated, removing certificate"
+			var err error
+			namespacedSecretName := types.NamespacedName{
+				Name:      watcher.ResolveTLSCertName(kyma.Name),
+				Namespace: "istio-system",
+			}
+			tlsSecret, err := GetTlsSecret(ctx, namespacedSecretName, kcpClient)
+			Expect(err).NotTo(HaveOccurred())
+
 			// The timeout used is 4 minutes bec the certificate gets rotated every 1 minute
-			Eventually(CheckPodLogs, 4*time.Minute).
+			Eventually(TlsSecretRotated, 4*time.Minute).
 				WithContext(ctx).
-				WithArguments(ControlPlaneNamespace, KLMPodPrefix, KLMPodContainer, expectedLogMessage, kcpRESTConfig,
-					kcpClient, timeNow).
+				WithArguments(tlsSecret.CreationTimestamp.Time, namespacedSecretName, kcpClient).
 				Should(Succeed())
 
 			By("And new TLS Certificate is created")
-			var err error
 			namespacedCertName := types.NamespacedName{
 				Name:      caCertName,
 				Namespace: "istio-system",
