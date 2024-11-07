b138639746adc87f4aa857511e064c451fdb9854
Badr, Nesma <nesma.badr@sap.com>
2024-11-07 11:15:01 +0100

    Debugging ModuleTemplate creation

--- scripts/tests/setup_https_server.sh
+++ scripts/tests/setup_https_server.sh
@@ -12,8 +12,37 @@ if [[ -z "$DIRECTORY_TO_SERVE" ]]; then
   exit 1
 fi
 
+cat <<EOF > openssl.cnf
+[ req ]
+default_bits       = 2048
+default_keyfile    = privkey.pem
+distinguished_name = req_distinguished_name
+x509_extensions    = v3_ca
+prompt              = no
+encrypt_key         = no
+
+[ req_distinguished_name ]
+C  = US
+ST = California
+L  = San Francisco
+O  = MyOrg
+OU = Dev
+CN = localhost  # Common Name, change to your domain/IP if needed
+
+[ v3_ca ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+DNS.1 = localhost
+IP.1 = 127.0.0.1
+EOF
+
+
 echo "Generating self-signed certificate..."
-openssl req -x509 -newkey rsa:2048 -keyout $KEY_FILE -out $CERT_FILE -days 30 -nodes -subj "/CN=localhost"
+openssl req -x509 -newkey rsa:2048 -keyout $KEY_FILE -out $CERT_FILE -days 30 -nodes -config openssl.cnf
+
+sudo cp $CERT_FILE /etc/ssl/certs/
+sudo update-ca-certificates
 
 # Start Python HTTPS server
 echo "Serving $DIRECTORY_TO_SERVE on https://localhost:$PORT"
