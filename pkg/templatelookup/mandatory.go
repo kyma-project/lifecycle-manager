95ca24d5384cec16ebadde653414ba5e8adad73f
Badr, Nesma <nesma.badr@sap.com>
2024-11-19 16:38:10 +0100

    add fieldselector

--- pkg/templatelookup/mandatory.go
+++ pkg/templatelookup/mandatory.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"k8s.io/apimachinery/pkg/fields"
 	k8slabels "k8s.io/apimachinery/pkg/labels"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -18,17 +17,18 @@ func GetMandatory(ctx context.Context, kymaClient client.Reader) (ModuleTemplate
 ) {
 	mandatoryModuleTemplateList := &v1beta2.ModuleTemplateList{}
 	labelSelector := k8slabels.SelectorFromSet(k8slabels.Set{shared.IsMandatoryModule: shared.EnableLabelValue})
-	fieldSelector := fields.SelectorFromSet(fields.Set{"metadata.deletionTimestamp": ""})
-	listOptions := &client.ListOptions{LabelSelector: labelSelector, FieldSelector: fieldSelector}
-	if err := kymaClient.List(ctx, mandatoryModuleTemplateList, listOptions); err != nil {
+	if err := kymaClient.List(ctx, mandatoryModuleTemplateList,
+		&client.ListOptions{LabelSelector: labelSelector}); err != nil {
 		return nil, fmt.Errorf("could not list mandatory ModuleTemplates: %w", err)
 	}
 
 	mandatoryModules := make(map[string]*ModuleTemplateInfo)
 	for _, moduleTemplate := range mandatoryModuleTemplateList.Items {
-		mandatoryModules[moduleTemplate.Name] = &ModuleTemplateInfo{
-			ModuleTemplate: &moduleTemplate,
-			Err:            nil,
+		if moduleTemplate.DeletionTimestamp.IsZero() {
+			mandatoryModules[moduleTemplate.Name] = &ModuleTemplateInfo{
+				ModuleTemplate: &moduleTemplate,
+				Err:            nil,
+			}
 		}
 	}
 	return mandatoryModules, nil
