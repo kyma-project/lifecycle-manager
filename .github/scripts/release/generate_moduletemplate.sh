cd68c3e9417620faf33b1100afe75be1657c2729
Xin Ruan <xin.ruan@sap.com>
2024-11-22 16:10:34 +0100

    use script to generate local module template with release meta

---
+++ .github/scripts/release/generate_moduletemplate.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Usage: This script is used for generating ModuleTemplate with a specific version for e2e test, before running the release script, you have to provide the defaultCR and manifest files in the specified paths, and start the https server to serve the files.
+set -o nounset
+set -o errexit
+set -E
+set -o pipefail
+
+RELEASE_VERSION=$1
+
+cat <<EOF > module-config.yaml
+name: kyma-project.io/module/template-operator
+version: ${RELEASE_VERSION}
+security: sec-scanners-config.yaml
+defaultCR: https://localhost:8080/config/samples/default-sample-cr.yaml
+manifest: https://localhost:8080/template-operator.yaml
+repository: https://github.com/kyma-project/template-operator
+documentation: https://github.com/kyma-project/template-operator/blob/main/README.md
+icons:
+- name: module-icon
+  link: https://github.com/kyma-project/template-operator/blob/main/docs/assets/logo.png"
+EOF
+
+modulectl create --config-file ./module-config.yaml --registry http://localhost:5111 --insecure
+sed -i 's/localhost:5111/k3d-kcp-registry.localhost:5000/g' ./template.yaml
+kubectl apply -f template.yaml
+
+echo "ModuleTemplate created successfully"
+
+cat <<EOF > module-release-meta.yaml
+apiVersion: operator.kyma-project.io/v1beta2
+kind: ModuleReleaseMeta
+metadata:
+  name: ${MODULE_NAME}
+  namespace: kcp-system
+spec:
+  channels:
+    - channel: regular
+      version: ${RELEASE_VERSION}
+  moduleName: ${MODULE_NAME}
+EOF
+kubectl apply -f module-release-meta.yaml
+
+echo "ModuleReleaseMeta created successfully"
