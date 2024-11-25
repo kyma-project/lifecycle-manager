1a187f00f5acc7e1906369423f80291d774b92b7
Xin Ruan <xin.ruan@sap.com>
2024-11-25 16:59:04 +0100

    add generate_modulereleasemeta.sh

--- .github/scripts/release/generate_moduletemplate.sh
+++ .github/scripts/release/generate_moduletemplate.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-# Usage: This script is used for generating ModuleTemplate with a specific version for e2e test, before running the release script, you have to provide the defaultCR and manifest files in the specified paths, and start the https server to serve the files.
 set -o nounset
 set -o errexit
 set -E
 set -o pipefail
 
-RELEASE_VERSION=$1
-MODULE_NAME=$2
+MODULE_NAME=$1
+RELEASE_VERSION=$2
 INCLUDE_DEFAULT_CR=${3:-true}
 
 cat <<EOF > module-config-for-e2e.yaml
@@ -33,22 +32,5 @@ kubectl apply -f template.yaml
 
 echo "ModuleTemplate created successfully"
 
-cat <<EOF > module-release-meta.yaml
-apiVersion: operator.kyma-project.io/v1beta2
-kind: ModuleReleaseMeta
-metadata:
-  name: ${MODULE_NAME}
-  namespace: kcp-system
-spec:
-  channels:
-  - channel: regular
-    version: ${RELEASE_VERSION}
-  moduleName: ${MODULE_NAME}
-EOF
-kubectl apply -f module-release-meta.yaml
-
-echo "ModuleReleaseMeta created successfully"
-
 rm -f module-config-for-e2e.yaml
-rm -f module-release-meta.yaml
 rm -f template.yaml
