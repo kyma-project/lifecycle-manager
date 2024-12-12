9f33e3313036c4cdd06f432ab89083d5d76ce325
Christoph Schwägerl <c.schwaegerl@sap.com>
2024-12-12 15:44:05 +0100

    chore: Use 'create_test_clusters.sh' for E2E tests

--- scripts/tests/create_test_clusters.sh
+++ scripts/tests/create_test_clusters.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-# Parse arguments
+# Initialize variables
 SKIP_VERSION_CHECK=false
-if [ "$1" == "--skip-version-check" ]; then
-  SKIP_VERSION_CHECK=true
-  shift
-fi
+K8S_VERSION=""
+CERT_MANAGER_VERSION=""
 
-# Check for invalid or extra arguments
-if [ $# -ne 0 ]; then
-  echo "[$(basename $0)] Invalid argument(s): $@"
-  echo "Usage: $(basename $0) [--skip-version-check]"
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --skip-version-check)
+      SKIP_VERSION_CHECK=true
+      shift
+      ;;
+    --k8s_version)
+      K8S_VERSION="$2"
+      shift 2
+      ;;
+    --cert_manager_version)
+      CERT_MANAGER_VERSION="$2"
+      shift 2
+      ;;
+    *)
+      echo "[$(basename $0)] Invalid argument: $1"
+      echo "Usage: $(basename $0) [--skip-version-check] --k8s_version <version> --cert_manager_version <version>"
+      exit 1
+      ;;
+  esac
+done
+
+# Check for mandatory arguments
+if [[ -z "$K8S_VERSION" || -z "$CERT_MANAGER_VERSION" ]]; then
+  echo "[$(basename $0)] Missing required arguments"
+  echo "Usage: $(basename $0) [--skip-version-check] --k8s_version <version> --cert_manager_version <version>"
   exit 1
 fi
 
@@ -29,12 +50,12 @@ fi
 # create SKR cluster
 if k3d cluster list | grep -q "^skr\s"; then
   echo "[$(basename $0)] Reusing existing SKR cluster..."
-  else
+else
   k3d cluster create skr \
         -p 10080:80@loadbalancer \
         -p 10443:443@loadbalancer \
         --k3s-arg --tls-san="skr.cluster.local@server:*" \
-        --image rancher/k3s:v1.28.7-k3s1 \
+        --image rancher/k3s:v${K8S_VERSION}-k3s1 \
         --k3s-arg --disable="traefik@server:*" \
         --k3s-arg --tls-san="host.k3d.internal@server:*" \
         --k3s-arg --tls-san="skr.cluster.local@server:*"
@@ -43,13 +64,13 @@ fi
 # create KCP cluster
 if k3d cluster list | grep -q "^kcp\s"; then
   echo "[$(basename $0)] Reusing existing KCP cluster..."
-  else
+else
   k3d cluster create kcp \
         -p 9443:443@loadbalancer \
         -p 9080:80@loadbalancer \
         -p 9081:8080@loadbalancer \
         --registry-create k3d-kcp-registry.localhost:5111 \
-        --image rancher/k3s:v1.28.7-k3s1 \
+        --image rancher/k3s:v${K8S_VERSION}-k3s1 \
         --k3s-arg --disable="traefik@server:*" \
         --k3s-arg --tls-san="host.k3d.internal@server:*" \
         --k3s-arg --tls-san="skr.cluster.local@server:*"
@@ -60,7 +81,7 @@ if k3d cluster list | grep -q "^kcp\s"; then
   istioctl install --set profile=demo -y
 
   # install cert-manager
-  kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.yaml
+  kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v${CERT_MANAGER_VERSION}/cert-manager.yaml
 
   ./add_skr_host_to_coredns.sh
 
