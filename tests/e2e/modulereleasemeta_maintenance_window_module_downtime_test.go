5944deeac501eba26bcc3165addea151861f09fa
Raj <me@hukumraj-singh-deora.party>
2025-01-16 16:45:26 +0100

    test: Add Concept for Maintenance Window with Module Downtime E2E Test

---
+++ tests/e2e/modulereleasemeta_maintenance_window_module_downtime_test.go
@@ -0,0 +1,139 @@
+package e2e_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/kyma-project/lifecycle-manager/api/shared"
+	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
+	. "github.com/kyma-project/lifecycle-manager/pkg/testutils"
+	. "github.com/kyma-project/lifecycle-manager/tests/e2e/commontestutils"
+)
+
+var _ = Describe("Maintenance Window With ModuleReleaseMeta and Module Downtime", Ordered, func() {
+	kyma := NewKymaWithSyncLabel("kyma-sample", ControlPlaneNamespace, v1beta2.DefaultChannel)
+	kyma.Labels["operator.kyma-project.io/region"] = "europe"
+	kyma.Spec.SkipMaintenanceWindows = false
+	/*
+		Maintenance Windows are defined as such:
+			region asia: current time - current time + 2 hours
+			region europe: tomorrow
+	*/
+
+	module := NewTemplateOperator(v1beta2.DefaultChannel)
+	moduleCR := NewTestModuleCR(RemoteNamespace)
+
+	InitEmptyKymaBeforeAll(kyma)
+	CleanupKymaAfterAll(kyma)
+
+	Context("Given SKR Cluster", func() {
+		It("When Kyma Module is enabled on SKR Kyma CR and Maintenance Window is opt in", func() {
+			Eventually(EnableModule).
+				WithContext(ctx).
+				WithArguments(skrClient, defaultRemoteKymaName, RemoteNamespace, module).
+				Should(Succeed())
+		})
+
+		It("Then Module CR is deployed", func() {
+			Eventually(ModuleCRExists).
+				WithContext(ctx).
+				WithArguments(skrClient, moduleCR).
+				Should(Succeed())
+
+			By("And Module Operator Deployment is deployed")
+			Eventually(DeploymentIsReady).
+				WithContext(ctx).
+				WithArguments(skrClient, ModuleDeploymentNameInOlderVersion, TestModuleResourceNamespace).
+				Should(Succeed())
+
+			By("And KCP Kyma CR is in \"Ready\" State")
+			Eventually(KymaIsInState).
+				WithContext(ctx).
+				WithArguments(kyma.GetName(), kyma.GetNamespace(), kcpClient, shared.StateReady).
+				Should(Succeed())
+		})
+
+		It("When new version requiring downtime is selected in ModuleReleaseMeta,"+
+			" but no Maintenance Window is active", func() {
+			Eventually(UpdateChannelVersionInModuleReleaseMeta).
+				WithContext(ctx).
+				WithArguments(kcpClient, module.Name, ControlPlaneNamespace, v1beta2.DefaultChannel, NewerVersion).
+				Should(Succeed())
+		})
+
+		It("Then Module CR exists", func() {
+			Eventually(ModuleCRExists).
+				WithContext(ctx).
+				WithArguments(skrClient, moduleCR).
+				Should(Succeed())
+
+			By("And old Module Operator Deployment still exists")
+			Eventually(DeploymentIsReady).
+				WithContext(ctx).
+				WithArguments(skrClient, ModuleDeploymentNameInOlderVersion, TestModuleResourceNamespace).
+				Should(Succeed())
+
+			By("And new Module Operator Deployment is not deployed")
+			Eventually(DeploymentIsReady).
+				WithContext(ctx).
+				WithArguments(skrClient, ModuleDeploymentNameInNewerVersion, TestModuleResourceNamespace).
+				Should(Equal(ErrNotFound))
+
+			By("And KCP Kyma CR is in \"Ready\" State")
+			Eventually(KymaIsInState).
+				WithContext(ctx).
+				WithArguments(kyma.GetName(), kyma.GetNamespace(), kcpClient, shared.StateReady).
+				Should(Succeed())
+		})
+
+		It("When a Maintenance Window is active", func() {
+			Eventually(UpdateKymaLabel).
+				WithContext(ctx).
+				WithArguments(kcpClient, kyma.Name, kyma.Namespace, "operator.kyma-project.io/region", "asia").
+				Should(Succeed())
+		})
+
+		It("Then Module CR exists", func() {
+			Eventually(ModuleCRExists).
+				WithContext(ctx).
+				WithArguments(skrClient, moduleCR).
+				Should(Succeed())
+
+			By("And old Module Operator Deployment no longer exists")
+			Eventually(DeploymentIsReady).
+				WithContext(ctx).
+				WithArguments(skrClient, ModuleDeploymentNameInOlderVersion, TestModuleResourceNamespace).
+				Should(Equal(ErrNotFound))
+
+			By("And new Module Operator Deployment is deployed")
+			Eventually(DeploymentIsReady).
+				WithContext(ctx).
+				WithArguments(skrClient, ModuleDeploymentNameInNewerVersion, TestModuleResourceNamespace).
+				Should(Succeed())
+
+			By("And KCP Kyma CR is in \"Ready\" State")
+			Eventually(KymaIsInState).
+				WithContext(ctx).
+				WithArguments(kyma.GetName(), kyma.GetNamespace(), kcpClient, shared.StateReady).
+				Should(Succeed())
+
+			By("And Kyma Module Version in Kyma Status is updated")
+			newModuleTemplateVersion, err := ReadModuleVersionFromModuleTemplate(ctx, kcpClient, module,
+				kyma.Spec.Channel, ControlPlaneNamespace)
+			Expect(err).ToNot(HaveOccurred())
+
+			Eventually(ModuleVersionInKymaStatusIsCorrect).
+				WithContext(ctx).
+				WithArguments(kcpClient, kyma.GetName(), kyma.GetNamespace(), module.Name,
+					newModuleTemplateVersion).
+				Should(Succeed())
+
+			By("And Manifest Version is updated")
+			Eventually(ManifestVersionIsCorrect).
+				WithContext(ctx).
+				WithArguments(kcpClient, kyma.GetName(), kyma.GetNamespace(), module.Name,
+					newModuleTemplateVersion).
+				Should(Succeed())
+		})
+	})
+})
