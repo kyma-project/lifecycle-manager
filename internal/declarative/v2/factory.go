0258ce64d9304c8638af7db5d81604bd3c478671
Raj <me@hukumraj-singh-deora.party>
2025-01-27 19:13:47 +0100

    fix: Memory Leak in KymaSkrContextProvider

--- internal/declarative/v2/factory.go
+++ internal/declarative/v2/factory.go
@@ -68,7 +68,7 @@ func NewSingletonClients(info *ClusterInfo) (*SingletonClients, error) {
 		return nil, err
 	}
 
-	// Required to prevent memory leak by avoiding caching in transport.tlsTransportCache, SingletonClients are cached anyways.
+	// Required to prevent memory leak by avoiding caching in transport.tlsTransportCache. SingletonClients are cached anyways.
 	info.Config.Proxy = http.ProxyFromEnvironment
 
 	httpClient, err := rest.HTTPClientFor(info.Config)
