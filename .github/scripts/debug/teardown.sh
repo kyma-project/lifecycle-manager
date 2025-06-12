24c856f27f8df7cb25ad435e8bde8406c2a80741
Benjamin Lindner <benjamin.lindner@sap.com>
2025-06-12 14:39:14 +0200

    chore: Add skr-webhook debug data to post teardown step in e2e

--- .github/scripts/debug/teardown.sh
+++ .github/scripts/debug/teardown.sh
@@ -8,3 +8,10 @@ echo "--- KLM POD ---"
 kubectl describe pod -n kcp-system --selector=app.kubernetes.io/name=kcp-lifecycle-manager
 echo "--- KLM LOGS ---"
 kubectl logs deploy/klm-controller-manager -n kcp-system --container manager
+
+kubectl config use-context k3d-skr
+echo "--- SKR-WEBHOOK LOGS ---"
+kubectl logs deploy/skr-webhook -n kyma-system --container server
+echo "--- SKR-WEBHOOK METRICS ---"
+kubectl port-forward svc/skr-webhook-metrics 8080:2112 -n kyma-system
+curl -s http://localhost:8080/metrics
