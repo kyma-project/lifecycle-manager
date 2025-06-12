1ed29e99f766f128dae310b8f28deb61033fe24b
Benjamin Lindner <benjamin.lindner@sap.com>
2025-06-12 10:56:17 +0200

    remove optional from boolean. does not have any effect

--- api/v1beta2/kyma_types.go
+++ api/v1beta2/kyma_types.go
@@ -51,7 +51,6 @@ type KymaSpec struct {
 
 	// SkipMaintenanceWindows indicates whether module upgrades that require downtime
 	// should bypass the defined Maintenance Windows and be applied immediately.
-	// +optional
 	SkipMaintenanceWindows bool `json:"skipMaintenanceWindows,omitempty"`
 
 	// Modules specifies the list of modules to be installed
