dce3c6443d8a5ba4cb772b63da1bbb37931e5b38
Raj <54686422+LeelaChacha@users.noreply.github.com>
2025-07-14 16:31:21 +0200

    Revert "fix: E2E Test for Legacy Istio Gateway Secret Rotation (#2582)"
    This reverts commit 4cfb8834c9cecac2c9ebdc829485ad1fef84d992.

--- api/shared/istio.go
+++ api/shared/istio.go
@@ -1,7 +1,6 @@
 package shared
 
 const (
-	CACertificateName        = "klm-watcher-serving"
 	IstioNamespace           = "istio-system"
 	GatewaySecretName        = "klm-istio-gateway" //nolint:gosec // It is just a name
 	LastModifiedAtAnnotation = "lastModifiedAt"
