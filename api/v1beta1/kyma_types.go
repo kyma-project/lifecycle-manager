5f03660d4caab3778a535590439318b947fd1181
Xin Ruan <xin.ruan@sap.com>
2025-08-04 14:34:05 +0200

    make v1beta1 immutable

--- api/v1beta1/kyma_types.go
+++ api/v1beta1/kyma_types.go
@@ -18,8 +18,6 @@ package v1beta1
 
 import (
 	apimetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
 )
 
 // +genclient
@@ -35,8 +33,8 @@ type Kyma struct {
 	apimetav1.TypeMeta   `json:",inline"`
 	apimetav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   KymaSpec           `json:"spec,omitempty"`
-	Status v1beta2.KymaStatus `json:"status,omitempty"`
+	Spec   KymaSpec   `json:"spec,omitempty"`
+	Status KymaStatus `json:"status,omitempty"`
 }
 
 // Sync defines settings used to apply the kyma synchronization to other clusters. This is defaulted to false
@@ -49,7 +47,7 @@ type Sync struct {
 
 	// +kubebuilder:default:=secret
 	// Strategy determines the way to look up the remotely synced kubeconfig, by default it is fetched from a secret
-	Strategy v1beta2.SyncStrategy `json:"strategy,omitempty"`
+	Strategy SyncStrategy `json:"strategy,omitempty"`
 
 	// The target namespace, if empty the namespace is reflected from the control plane
 	// Note that cleanup is currently not supported if you are switching the namespace, so you will
@@ -66,6 +64,10 @@ type Sync struct {
 	// to be synchronized for discovery purposes
 	ModuleCatalog bool `json:"moduleCatalog,omitempty"`
 }
+
+// SyncStrategy determines how the Remote Cluster is synchronized with the Control Plane. This can influence secret
+// lookup, or other behavioral patterns when interacting with the remote cluster.
+type SyncStrategy string
 
 // KymaSpec defines the desired state of Kyma.
 type KymaSpec struct {
@@ -85,6 +87,98 @@ type KymaSpec struct {
 	// Active Synchronization Settings
 	// +optional
 	Sync Sync `json:"sync,omitempty"`
+}
+
+// KymaStatus defines the observed state of Kyma.
+type KymaStatus struct {
+	// State signifies current state of Kyma.
+	// Value can be one of ("Ready", "Processing", "Error", "Deleting").
+	State State `json:"state,omitempty"`
+
+	// List of status conditions to indicate the status of a ServiceInstance.
+	// +optional
+	// +listType=map
+	// +listMapKey=type
+	Conditions []apimetav1.Condition `json:"conditions,omitempty"`
+
+	// Contains essential information about the current deployed module
+	Modules []ModuleStatus `json:"modules,omitempty"`
+
+	// Active Channel
+	// +optional
+	ActiveChannel string `json:"activeChannel,omitempty"`
+
+	LastOperation `json:"lastOperation,omitempty"`
+}
+
+type ModuleStatus struct {
+	// Name defines the name of the Module in the Spec that the status is used for.
+	// It can be any kind of Reference format supported by Module.Name.
+	Name string `json:"name"`
+
+	// FQDN is the fully qualified domain name of the module.
+	// In the ModuleTemplate it is located in .spec.descriptor.component.name of the ModuleTemplate
+	// FQDN is used to calculate Namespace and Name of the Manifest for tracking.
+	FQDN string `json:"fqdn,omitempty"`
+
+	// Channel tracks the active Channel of the Module. In Case it changes, the new Channel will have caused
+	// a new lookup to be necessary that maybe picks a different ModuleTemplate, which is why we need to reconcile.
+	Channel string `json:"channel,omitempty"`
+
+	// Channel tracks the active Version of the Module.
+	Version string `json:"version,omitempty"`
+
+	// Message is a human-readable message indicating details about the State.
+	Message string `json:"message,omitempty"`
+
+	// State of the Module in the currently tracked Generation
+	State State `json:"state"`
+
+	// Manifest contains the Information of a related Manifest
+	Manifest *TrackingObject `json:"manifest,omitempty"`
+
+	// Resource contains information about the created module CR.
+	Resource *TrackingObject `json:"resource,omitempty"`
+
+	// It contains information about the last parsed ModuleTemplate in Context of the Installation.
+	// This will update when Channel or the ModuleTemplate is changed.
+	// +optional
+	Template *TrackingObject `json:"template,omitempty"`
+}
+
+// TrackingObject contains TypeMeta and PartialMeta to allow a generation based object tracking.
+// It purposefully does not use ObjectMeta as the generation of controller-runtime for crds would not validate
+// the generation fields even when embedding ObjectMeta.
+type TrackingObject struct {
+	apimetav1.TypeMeta `json:",inline"`
+	PartialMeta        `json:"metadata,omitempty"`
+}
+
+// PartialMeta is a subset of ObjectMeta that contains relevant information to track an Object.
+// see https://github.com/kubernetes/apimachinery/blob/v0.26.1/pkg/apis/meta/v1/types.go#L111
+type PartialMeta struct {
+	// Name must be unique within a namespace. Is required when creating resources, although
+	// some resources may allow a client to request the generation of an appropriate name
+	// automatically. Name is primarily intended for creation idempotence and configuration
+	// definition.
+	// Cannot be updated.
+	// More info: http://kubernetes.io/docs/user-guide/identifiers#names
+	// +optional
+	Name string `json:"name"`
+	// Namespace defines the space within which each name must be unique. An empty namespace is
+	// equivalent to the "default" namespace, but "default" is the canonical representation.
+	// Not all objects are required to be scoped to a namespace - the value of this field for
+	// those objects will be empty.
+	//
+	// Must be a DNS_LABEL.
+	// Cannot be updated.
+	// More info: http://kubernetes.io/docs/user-guide/namespaces
+	// +optional
+	Namespace string `json:"namespace"`
+	// A sequence number representing a specific generation of the desired state.
+	// Populated by the system. Read-only.
+	// +optional
+	Generation int64 `json:"generation,omitempty"`
 }
 
 // +kubebuilder:object:root=true
