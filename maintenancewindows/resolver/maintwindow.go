b9424beb738b1b7cc80fb2290285e307a9d472f8
Xin Ruan <xin.ruan@sap.com>
2025-09-18 14:50:27 +0200

    Refactors resource list methods and improves test coverage
    Aligns resource list logic with upstream by renaming methods,
    adding method exclusions for static analysis, and updating
    receiver types for consistency and clarity.
    
    Introduces unit tests to validate resource list difference
    functionality, enhancing reliability.
    
    Refactors maintenance window helpers to use pointer receivers,
    improving method semantics and future extensibility.

--- maintenancewindows/resolver/maintwindow.go
+++ maintenancewindows/resolver/maintwindow.go
@@ -272,15 +272,15 @@ func (r *Regexp) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-func (r Regexp) MatchString(s string) bool {
+func (r *Regexp) MatchString(s string) bool {
 	return r.Regexp.MatchString(s)
 }
 
-func (r Regexp) IsValid() bool {
+func (r *Regexp) IsValid() bool {
 	return r.Regexp != nil
 }
 
-func (r Regexp) String() string {
+func (r *Regexp) String() string {
 	return r.Str
 }
 
@@ -368,8 +368,8 @@ func (wt *WindowTime) UnmarshalJSON(data []byte) error {
 	}
 }
 
-func (wt WindowTime) T() time.Time {
-	return time.Time(wt)
+func (wt *WindowTime) T() time.Time {
+	return time.Time(*wt)
 }
 
 // utility functions.
