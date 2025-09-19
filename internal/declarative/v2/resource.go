e062a1ed345147695777d85b6fe0bd0811bde658
Xin Ruan <xin.ruan@sap.com>
2025-09-19 09:43:55 +0200

    chore: Enable `recvcheck` (#2757)
    * Refactors resource list methods and improves test coverage
    
    Aligns resource list logic with upstream by renaming methods,
    adding method exclusions for static analysis, and updating
    receiver types for consistency and clarity.
    
    Introduces unit tests to validate resource list difference
    functionality, enhancing reliability.
    
    Refactors maintenance window helpers to use pointer receivers,
    improving method semantics and future extensibility.
    
    * Restores append method documentation for clarity
    
    Moves the append method comment to its correct location,
    improving code readability and aligning documentation with implementation.
    
    * Excludes deepcopy methods from linting checks
    
    Prevents false positives and unnecessary lint warnings for auto-generated deepcopy functions, improving lint signal quality and reducing maintenance overhead.

--- internal/declarative/v2/resource.go
+++ internal/declarative/v2/resource.go
@@ -2,12 +2,14 @@ package v2
 
 import "k8s.io/cli-runtime/pkg/resource"
 
-// ResourceList provides convenience methods for comparing collections of Infos. Copy from helm.sh/helm/v3/pkg/kube.
+// ResourceList provides convenience methods for comparing collections of Infos. Copy from https://github.com/helm/helm/blob/v3.19.0/pkg/kube/resource.go
 type ResourceList []*resource.Info
 
-// Append adds an Info to the Result.
-func (r *ResourceList) Append(val *resource.Info) {
-	*r = append(*r, val)
+// Difference will return a new Result with objects not contained in rs.
+func (r ResourceList) Difference(rs ResourceList) ResourceList {
+	return r.filter(func(info *resource.Info) bool {
+		return !rs.contains(info)
+	})
 }
 
 // Visit implements resource.Visitor.
@@ -20,47 +22,30 @@ func (r ResourceList) Visit(fn resource.VisitorFunc) error {
 	return nil
 }
 
-// Filter returns a new Result with Infos that satisfy the predicate fn.
-func (r ResourceList) Filter(fn func(*resource.Info) bool) ResourceList {
+// append adds an Info to the Result.
+func (r *ResourceList) append(val *resource.Info) {
+	*r = append(*r, val)
+}
+
+// filter returns a new Result with Infos that satisfy the predicate fn.
+func (r ResourceList) filter(fn func(*resource.Info) bool) ResourceList {
 	var result ResourceList
 	for _, i := range r {
 		if fn(i) {
-			result.Append(i)
+			result.append(i)
 		}
 	}
 	return result
 }
 
-// Get returns the Info from the result that matches the name and kind.
-func (r ResourceList) Get(info *resource.Info) *resource.Info {
-	for _, i := range r {
-		if isMatchingInfo(i, info) {
-			return i
-		}
-	}
-	return nil
-}
-
-// Contains checks to see if an object exists.
-func (r ResourceList) Contains(info *resource.Info) bool {
+// contains checks to see if an object exists.
+func (r ResourceList) contains(info *resource.Info) bool {
 	for _, i := range r {
 		if isMatchingInfo(i, info) {
 			return true
 		}
 	}
 	return false
-}
-
-// Difference will return a new Result with objects not contained in rs.
-func (r ResourceList) Difference(rs ResourceList) ResourceList {
-	return r.Filter(func(info *resource.Info) bool {
-		return !rs.Contains(info)
-	})
-}
-
-// Intersect will return a new Result with objects contained in both Results.
-func (r ResourceList) Intersect(rs ResourceList) ResourceList {
-	return r.Filter(rs.Contains)
 }
 
 // isMatchingInfo returns true if infos match on Name and GroupVersionKind.
