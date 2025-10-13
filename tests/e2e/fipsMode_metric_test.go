2265390dd38e968789fca732f66ef6673ce98a64
Xin Ruan <xin.ruan@sap.com>
2025-10-13 10:19:27 +0200

    test: Add verification for GODEBUG environment variable in SKR Cluster webhook deployment

--- tests/e2e/fipsMode_metric_test.go
+++ tests/e2e/fipsMode_metric_test.go
@@ -1,12 +1,16 @@
 package e2e_test
 
 import (
+	"fmt"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	. "github.com/kyma-project/lifecycle-manager/pkg/testutils"
 	. "github.com/kyma-project/lifecycle-manager/tests/e2e/commontestutils"
 
 	"github.com/kyma-project/lifecycle-manager/internal/pkg/metrics"
+	skrwebhookresources "github.com/kyma-project/lifecycle-manager/internal/service/watcher/resources"
 )
 
 var _ = Describe("FIPS Mode metric", Ordered, func() {
@@ -16,6 +20,28 @@ var _ = Describe("FIPS Mode metric", Ordered, func() {
 			Eventually(GetFipsModeGauge).
 				WithContext(ctx).
 				Should(Equal(metrics.FipsModeOnly))
+		})
+	})
+
+	Context("Given SKR Cluster", func() {
+		It("When Runtime Watcher is initialized", func() {
+			By("Then fipsMode env exists in the webhook deployment")
+			Eventually(func() error {
+				skrWebhook, err := GetDeployment(ctx, skrClient, skrwebhookresources.SkrResourceName, RemoteNamespace)
+				if err != nil {
+					return err
+				}
+				for _, container := range skrWebhook.Spec.Template.Spec.Containers {
+					if container.Name == "server" {
+						for _, env := range container.Env {
+							if env.Name == "GODEBUG" && env.Value == "fips140=only,tlsmlkem=0" {
+								return nil
+							}
+						}
+					}
+				}
+				return fmt.Errorf("GODEBUG env not found in the webhook deployment")
+			}).Should(Succeed())
 		})
 	})
 })
