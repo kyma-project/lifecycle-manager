cb07d68ba626058b60b8babe3bc0a97013cda9a1
Benjamin Lindner <benjamin.lindner@sap.com>
2025-10-30 12:38:32 +0100

    fix

--- api/v1beta2/condition_messages.go
+++ api/v1beta2/condition_messages.go
@@ -4,28 +4,28 @@ import (
 	apimetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+//nolint:gosec // no secrets defined in these consts
 const (
 	// DeprecatedConditionTypeReady was introduced by a bug. Ths condition needs to be removed on all KymaCRs.
 	DeprecatedConditionTypeReady KymaConditionType = "Ready"
 	ConditionTypeModules         KymaConditionType = "Modules"
 	ConditionTypeModuleCatalog   KymaConditionType = "ModuleCatalog"
 	ConditionTypeSKRWebhook      KymaConditionType = "SKRWebhook"
-	//nolint:gosec // not actually a real secret
+
 	ConditionTypeSKRImagePullSecretSync KymaConditionType = "SKRImagePullSecretSync"
 
 	// ConditionReason will be set to `Ready` on all Conditions. If the Condition is actual ready,
 	// can be determined by the state.
 	ConditionReason KymaConditionReason = "Ready"
 
-	ConditionMessageModuleInReadyState        = "all modules are in ready state"
-	ConditionMessageModuleNotInReadyState     = "not all modules are in ready state"
-	ConditionMessageModuleCatalogIsSynced     = "module templates are synchronized"
-	ConditionMessageModuleCatalogIsOutOfSync  = "module templates are out of sync and need to be resynchronized"
-	ConditionMessageModuleStateUnknown        = "modules state is unknown"
-	ConditionMessageModuleCatalogStateUnknown = "module templates synchronization state is unknown"
-	ConditionMessageSKRWebhookIsSynced        = "skrwebhook is synchronized"
-	ConditionMessageSKRWebhookIsOutOfSync     = "skrwebhook is out of sync and needs to be resynchronized"
-	//nolint:gosec // not actually a real secret
+	ConditionMessageModuleInReadyState          = "all modules are in ready state"
+	ConditionMessageModuleNotInReadyState       = "not all modules are in ready state"
+	ConditionMessageModuleCatalogIsSynced       = "module templates are synchronized"
+	ConditionMessageModuleCatalogIsOutOfSync    = "module templates are out of sync and need to be resynchronized"
+	ConditionMessageModuleStateUnknown          = "modules state is unknown"
+	ConditionMessageModuleCatalogStateUnknown   = "module templates synchronization state is unknown"
+	ConditionMessageSKRWebhookIsSynced          = "skrwebhook is synchronized"
+	ConditionMessageSKRWebhookIsOutOfSync       = "skrwebhook is out of sync and needs to be resynchronized"
 	ConditionMessageSKRImagePullSecretSynced    = "skr image pull secret is synchronized"
 	ConditionMessageSKRImagePullSecretOutOfSync = "skr image pull secret is out of sync and needs to be resynchronized"
 )
