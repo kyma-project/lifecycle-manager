647f56db1b0bbc8175b5d81a697cbc7e7f0d28b7
Christoph Schwägerl <c.schwaegerl@sap.com>
2025-11-26 08:58:41 +0100

    lint

--- internal/service/kyma/deletion/usecases/set_skr_kyma_state_deleting_test.go
+++ internal/service/kyma/deletion/usecases/set_skr_kyma_state_deleting_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	apicorev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	apimetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -15,8 +17,6 @@ import (
 	"github.com/kyma-project/lifecycle-manager/internal/result/kyma/usecase"
 	"github.com/kyma-project/lifecycle-manager/internal/service/kyma/deletion/usecases"
 	"github.com/kyma-project/lifecycle-manager/pkg/testutils/random"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestExecute_Succeeds(t *testing.T) {
@@ -241,7 +241,9 @@ type skrKymaStatusRepoStub struct {
 	err            error
 }
 
-func (r *skrKymaStatusRepoStub) Get(_ context.Context, namespacedName types.NamespacedName) (*v1beta2.KymaStatus, error) {
+func (r *skrKymaStatusRepoStub) Get(_ context.Context,
+	namespacedName types.NamespacedName,
+) (*v1beta2.KymaStatus, error) {
 	r.called = true
 	r.namespacedName = namespacedName
 	return r.status, r.err
