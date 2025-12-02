ac066ed15a0f45bc85fb99eb93d93e4de360d847
Benjamin Lindner <benjamin.lindner@sap.com>
2025-12-02 09:10:47 +0100

    extend repo and tests

--- internal/repository/skr/webhook/skrwebhook_repo.go
+++ internal/repository/skr/webhook/skrwebhook_repo.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 
 	"golang.org/x/sync/errgroup"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	skrwebhookresources "github.com/kyma-project/lifecycle-manager/internal/service/watcher/resources"
 	"github.com/kyma-project/lifecycle-manager/pkg/util"
 )
 
@@ -17,19 +20,51 @@ type SkrClientCache interface {
 	Get(key client.ObjectKey) client.Client
 }
 
+// ResourceRef contains the minimal information needed to identify and delete a resource.
+type ResourceRef struct {
+	Name       string
+	APIVersion string
+	Kind       string
+}
+
 type ResourceRepository struct {
-	resources           []*unstructured.Unstructured
+	resources           []ResourceRef
 	skrClientCache      SkrClientCache
 	remoteSyncNamespace string
 }
 
 func NewResourceRepository(
-	webhookResources []*unstructured.Unstructured,
 	skrClientCache SkrClientCache,
 	remoteSyncNamespace string,
+	baseResources []*unstructured.Unstructured,
 ) *ResourceRepository {
+	resources := make([]ResourceRef, 0, len(baseResources)+2)
+	for _, res := range baseResources {
+		gvk := res.GroupVersionKind()
+
+		resources = append(resources, ResourceRef{
+			Name:       res.GetName(),
+			APIVersion: gvk.GroupVersion().String(),
+			Kind:       gvk.Kind,
+		})
+	}
+
+	// Add generated resources that are created dynamically from SkrWebhookManifestManager (not read from resources.yaml)
+	resources = append(resources,
+		ResourceRef{
+			Name:       skrwebhookresources.SkrResourceName,
+			APIVersion: admissionregistrationv1.SchemeGroupVersion.String(),
+			Kind:       "ValidatingWebhookConfiguration",
+		},
+		ResourceRef{
+			Name:       skrwebhookresources.SkrTLSName,
+			APIVersion: "v1",
+			Kind:       "Secret",
+		},
+	)
+
 	return &ResourceRepository{
-		resources:           webhookResources,
+		resources:           resources,
 		skrClientCache:      skrClientCache,
 		remoteSyncNamespace: remoteSyncNamespace,
 	}
@@ -46,6 +81,7 @@ func (r *ResourceRepository) ResourcesExist(kymaName string) (bool, error) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	errGrp, grpCtx := errgroup.WithContext(ctx)
 	resourceExists := make(chan bool, 1)
 
@@ -59,18 +95,20 @@ func (r *ResourceRepository) ResourcesExist(kymaName string) (bool, error) {
 			default:
 			}
 
-			resource := r.resources[resIdx].DeepCopy()
+			ref := r.resources[resIdx]
+			resource := &unstructured.Unstructured{}
+			resource.SetGroupVersionKind(schema.FromAPIVersionAndKind(ref.APIVersion, ref.Kind))
+			resource.SetName(ref.Name)
 			resource.SetNamespace(r.remoteSyncNamespace)
 
 			err := skrClient.Get(grpCtx, client.ObjectKeyFromObject(resource), resource)
 			if err != nil {
 				if apierrors.IsNotFound(err) {
-					// Resource does not exist - continue checking others
+					// Resource does not exist, continue checking others
 					return nil
 				}
-				return fmt.Errorf("failed to check resource %s: %w", resource.GetName(), err)
+				return fmt.Errorf("failed to check resource %s: %w", ref.Name, err)
 			}
-			// Resource exists - signal and cancel context to stop other checks
 			select {
 			case resourceExists <- true:
 				cancel()
@@ -102,15 +140,19 @@ func (r *ResourceRepository) DeleteWebhookResources(ctx context.Context, kymaNam
 	skrClient := r.skrClientCache.Get(kymaNamespacedName)
 
 	errGrp, grpCtx := errgroup.WithContext(ctx)
+
 	for idx := range r.resources {
 		resIdx := idx
 		errGrp.Go(func() error {
-			resource := r.resources[resIdx].DeepCopy()
+			ref := r.resources[resIdx]
+			resource := &unstructured.Unstructured{}
+			resource.SetGroupVersionKind(schema.FromAPIVersionAndKind(ref.APIVersion, ref.Kind))
+			resource.SetName(ref.Name)
 			resource.SetNamespace(r.remoteSyncNamespace)
 
 			err := skrClient.Delete(grpCtx, resource)
 			if err != nil {
-				return fmt.Errorf("failed to delete resource %s: %w", resource.GetName(), err)
+				return fmt.Errorf("failed to delete resource %s: %w", ref.Name, err)
 			}
 			return nil
 		})
