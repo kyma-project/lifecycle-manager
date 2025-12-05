ad5c63eabd4062d130bb23d4872257003ac4ff99
Raj <me@hukumraj-singh-deora.party>
2025-12-05 21:22:47 +0100

    fix: Revert back to old Kyma

--- internal/controller/kyma/setup.go
+++ internal/controller/kyma/setup.go
@@ -1,13 +1,19 @@
 package kyma
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 
 	apicorev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	ctrlruntime "sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/source"
@@ -28,6 +34,12 @@ type SetupOptions struct {
 }
 
 const controllerName = "kyma"
+
+var (
+	errConvertingWatched      = errors.New("error converting watched to object key")
+	errParsingWatched         = errors.New("error getting watched object from unstructured event")
+	errConvertingWatcherEvent = errors.New("error converting watched object to unstructured event")
+)
 
 func (r *Reconciler) SetupWithManager(mgr ctrl.Manager, opts ctrlruntime.Options, settings SetupOptions) error {
 	var verifyFunc watcherevent.Verify
@@ -59,10 +71,47 @@ func (r *Reconciler) SetupWithManager(mgr ctrl.Manager, opts ctrlruntime.Options
 		Watches(&v1beta2.Manifest{},
 			handler.EnqueueRequestForOwner(mgr.GetScheme(), mgr.GetRESTMapper(), &v1beta2.Kyma{},
 				handler.OnlyControllerOwner()), builder.WithPredicates(predicate.ResourceVersionChangedPredicate{})).
-		WatchesRawSource(source.Channel(controller.AdaptEvents(runnableListener.ReceivedEvents), NewSkrEventHandler())).
+		WatchesRawSource(source.Channel(controller.AdaptEvents(runnableListener.ReceivedEvents), r.skrEventHandler())).
 		Complete(r); err != nil {
 		return fmt.Errorf("failed to setup manager for kyma controller: %w", err)
 	}
 
 	return nil
+}
+
+func (r *Reconciler) skrEventHandler() *handler.Funcs {
+	return &handler.Funcs{
+		GenericFunc: func(ctx context.Context, evnt event.GenericEvent,
+			queue workqueue.TypedRateLimitingInterface[ctrl.Request],
+		) {
+			logger := ctrl.Log.WithName("listener")
+			unstructWatcherEvt, conversionOk := evnt.Object.(*unstructured.Unstructured)
+			if !conversionOk {
+				logger.Error(errConvertingWatcherEvent, fmt.Sprintf("event: %v", evnt.Object))
+				return
+			}
+
+			// get owner object from unstructured event, owner = KymaCR object reference in KCP
+			unstructuredOwner, ok := unstructWatcherEvt.Object["owner"]
+			if !ok {
+				logger.Error(errParsingWatched, fmt.Sprintf("unstructured event: %v", unstructWatcherEvt))
+				return
+			}
+
+			ownerObjectKey, conversionOk := unstructuredOwner.(client.ObjectKey)
+			if !conversionOk {
+				logger.Error(errConvertingWatched, fmt.Sprintf("unstructured Owner object: %v", unstructuredOwner))
+				return
+			}
+
+			logger.Info(
+				fmt.Sprintf("event received from SKR, adding %s to queue",
+					ownerObjectKey),
+			)
+
+			queue.Add(ctrl.Request{
+				NamespacedName: ownerObjectKey,
+			})
+		},
+	}
 }
