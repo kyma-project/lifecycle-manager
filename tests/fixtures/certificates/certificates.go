afa58aa57f2f7967a5cc173009eb5c2dfb30e47f
Christoph Schwägerl <c.schwaegerl@sap.com>
2026-02-06 09:05:09 +0100

    add cert validities

--- tests/fixtures/certificates/certificates.go
+++ tests/fixtures/certificates/certificates.go
@@ -5,13 +5,13 @@ import (
 )
 
 //go:embed certificate-valid-1.pem
-var Cert1 []byte
+var Cert1 []byte // valid until Feb  2 10:17:07 2036 GMT
 
 //go:embed certificate-valid-2.pem
-var Cert2 []byte
+var Cert2 []byte // valid until Feb  2 10:18:00 2036 GMT
 
 //go:embed certificate-valid-3.pem
-var Cert3 []byte
+var Cert3 []byte // valid until Feb  2 12:00:19 2036 GMT
 
 //go:embed certificate-expired.pem
-var CertExpired []byte
+var CertExpired []byte // expired on Feb  5 10:24:41 2026 GMT
