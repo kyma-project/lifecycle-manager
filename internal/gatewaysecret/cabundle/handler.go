358b7c190a98097ee660c3612d5c11087a8bf837
c-pius <c.schwaegerl@sap.com>
2026-02-27 11:52:12 +0100

    lint

--- internal/gatewaysecret/cabundle/handler.go
+++ internal/gatewaysecret/cabundle/handler.go
@@ -8,12 +8,12 @@ import (
 
 	apicorev1 "k8s.io/api/core/v1"
 	apimetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/kyma-project/lifecycle-manager/api/shared"
 	"github.com/kyma-project/lifecycle-manager/internal/gatewaysecret"
 	"github.com/kyma-project/lifecycle-manager/pkg/log"
 	"github.com/kyma-project/lifecycle-manager/pkg/util"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var ErrCACertificateNotReady = errors.New("watcher-serving ca certificate is not ready")
