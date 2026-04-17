f19104f79340437747202a67fd8df3940f488ec4
Raj <me@hukumraj-singh-deora.party>
2026-04-17 15:19:43 +0200

    chore: Finalize PR 2

--- internal/watch/modulereleasemeta/events/events_test.go
+++ internal/watch/modulereleasemeta/events/events_test.go
@@ -1,11 +1,10 @@
 package events_test
 
 import (
-	"reflect"
-	"slices"
 	"sort"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	apimetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 
@@ -13,307 +12,278 @@ import (
 	"github.com/kyma-project/lifecycle-manager/internal/watch/modulereleasemeta/events"
 )
 
-func Test_DiffModuleReleaseMetaChannels(t *testing.T) {
-	type args struct {
-		oldModuleReleaseMeta *v1beta2.ModuleReleaseMeta
-		newModuleReleaseMeta *v1beta2.ModuleReleaseMeta
-	}
+func Test_AffectedKymasOnDelete(t *testing.T) {
 	tests := []struct {
-		name string
-		args args
-		want []string
+		name  string
+		mrm   *v1beta2.ModuleReleaseMeta
+		kymas *v1beta2.KymaList
+		want  []*types.NamespacedName
 	}{
 		{
-			name: "Empty input",
-			args: args{
-				oldModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{},
-						},
-					},
-				},
-				newModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{},
-						},
+			name: "no kymas present",
+			mrm: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels: []v1beta2.ChannelVersionAssignment{
+						{Channel: "regular", Version: "1.0.0"},
 					},
 				},
 			},
-			want: nil,
+			kymas: &v1beta2.KymaList{},
+			want:  []*types.NamespacedName{},
 		},
 		{
-			name: "No difference same channels",
-			args: args{
-				oldModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.0.0"},
-						},
-					},
-				},
-				newModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.0.0"},
-						},
+			name: "kyma with affected module is requeued",
+			mrm: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels: []v1beta2.ChannelVersionAssignment{
+						{Channel: "regular", Version: "1.0.0"},
 					},
 				},
 			},
-			want: nil,
+			kymas: &v1beta2.KymaList{
+				Items: []v1beta2.Kyma{
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-1", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "regular"}}},
+					},
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-2", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "other", Channel: "regular"}}},
+					},
+				},
+			},
+			want: []*types.NamespacedName{{Name: "kyma-1", Namespace: "kcp-system"}},
 		},
 		{
-			name: "One channel updated",
-			args: args{
-				oldModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.0.0"},
-							{Channel: "fast", Version: "2.0.0"},
-						},
-					},
-				},
-				newModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.1.0"},
-							{Channel: "fast", Version: "2.0.0"},
-						},
+			name: "all kymas with module across all channels requeued on delete",
+			mrm: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels: []v1beta2.ChannelVersionAssignment{
+						{Channel: "regular", Version: "1.0.0"},
+						{Channel: "fast", Version: "2.0.0"},
 					},
 				},
 			},
-			want: []string{"regular"},
+			kymas: &v1beta2.KymaList{
+				Items: []v1beta2.Kyma{
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-1", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "regular"}}},
+					},
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-2", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "fast"}}},
+					},
+				},
+			},
+			want: []*types.NamespacedName{
+				{Name: "kyma-1", Namespace: "kcp-system"},
+				{Name: "kyma-2", Namespace: "kcp-system"},
+			},
 		},
 		{
-			name: "All channels updated",
-			args: args{
-				oldModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.0.0"},
-							{Channel: "fast", Version: "2.0.0"},
-						},
-					},
-				},
-				newModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.1.0"},
-							{Channel: "fast", Version: "2.1.0"},
-						},
+			name: "kyma channel falls back to kyma spec channel when module channel is empty",
+			mrm: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels: []v1beta2.ChannelVersionAssignment{
+						{Channel: "regular", Version: "1.0.0"},
 					},
 				},
 			},
-			want: []string{"fast", "regular"},
-		},
-		{
-			name: "New channel added",
-			args: args{
-				oldModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.0.0"},
-						},
-					},
-				},
-				newModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.0.0"},
-							{Channel: "fast", Version: "2.0.0"},
-						},
+			kymas: &v1beta2.KymaList{
+				Items: []v1beta2.Kyma{
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-1", Namespace: "kcp-system"},
+						Spec:       v1beta2.KymaSpec{Channel: "regular"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: ""}}},
 					},
 				},
 			},
-			want: []string{"fast"},
-		},
-		{
-			name: "Channel removed",
-			args: args{
-				oldModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.0.0"},
-						},
-					},
-				},
-				newModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{},
-					},
-				},
-			},
-			want: []string{"regular"},
-		},
-		{
-			name: "Multiple channels added and updated",
-			args: args{
-				oldModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.0.0"},
-						},
-					},
-				},
-				newModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.1.0"},
-							{Channel: "fast", Version: "2.0.0"},
-							{Channel: "experimental", Version: "3.0.0"},
-						},
-					},
-				},
-			},
-			want: []string{"experimental", "fast", "regular"},
-		},
-		{
-			name: "Multiple channels removed",
-			args: args{
-				oldModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{
-							{Channel: "regular", Version: "1.0.0"},
-							{Channel: "fast", Version: "2.0.0"},
-						},
-					},
-				},
-				newModuleReleaseMeta: &v1beta2.ModuleReleaseMeta{
-					Spec: v1beta2.ModuleReleaseMetaSpec{
-						Channels: []v1beta2.ChannelVersionAssignment{},
-					},
-				},
-			},
-			want: []string{"fast", "regular"},
+			want: []*types.NamespacedName{{Name: "kyma-1", Namespace: "kcp-system"}},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := events.DiffModuleReleaseMetaChannels(tt.args.oldModuleReleaseMeta, tt.args.newModuleReleaseMeta)
-			sort.Strings(got)
-			if !slices.Equal(got, tt.want) {
-				t.Errorf("DiffModuleReleaseMetaChannels() = %v, want %v", got, tt.want)
-			}
+			got := events.AffectedKymasOnDelete(tt.mrm, tt.kymas)
+			require.Equal(t, tt.want, got)
 		})
 	}
 }
 
-func Test_GetAffectedKymas(t *testing.T) {
-	type args struct {
-		kymas            *v1beta2.KymaList
-		moduleName       string
-		affectedChannels []string
-	}
+func Test_AffectedKymasOnUpdate(t *testing.T) {
 	tests := []struct {
-		name string
-		args args
-		want []*types.NamespacedName
+		name   string
+		oldMRM *v1beta2.ModuleReleaseMeta
+		newMRM *v1beta2.ModuleReleaseMeta
+		kymas  *v1beta2.KymaList
+		want   []*types.NamespacedName
 	}{
 		{
-			name: "No Kymas present",
-			args: args{
-				kymas:            &v1beta2.KymaList{},
-				affectedChannels: []string{},
+			name: "no channel changes - no kymas requeued",
+			oldMRM: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels:   []v1beta2.ChannelVersionAssignment{{Channel: "regular", Version: "1.0.0"}},
+				},
+			},
+			newMRM: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels:   []v1beta2.ChannelVersionAssignment{{Channel: "regular", Version: "1.0.0"}},
+				},
+			},
+			kymas: &v1beta2.KymaList{
+				Items: []v1beta2.Kyma{
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-1", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "regular"}}},
+					},
+				},
 			},
 			want: []*types.NamespacedName{},
 		},
 		{
-			name: "Single update affects one Kyma",
-			args: args{
-				kymas: &v1beta2.KymaList{
-					Items: []v1beta2.Kyma{
-						{
-							ObjectMeta: apimetav1.ObjectMeta{Name: "test-kyma-1", Namespace: "kcp-system"},
-							Status: v1beta2.KymaStatus{
-								Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "regular"}},
-							},
-						},
-						{
-							ObjectMeta: apimetav1.ObjectMeta{Name: "test-kyma-2", Namespace: "kcp-system"},
-							Status: v1beta2.KymaStatus{
-								Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "fast"}},
-							},
-						},
+			name: "updated channel version requeues affected kyma",
+			oldMRM: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels: []v1beta2.ChannelVersionAssignment{
+						{Channel: "regular", Version: "1.0.0"},
+						{Channel: "fast", Version: "2.0.0"},
 					},
 				},
-				moduleName:       "module",
-				affectedChannels: []string{"regular"},
+			},
+			newMRM: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels: []v1beta2.ChannelVersionAssignment{
+						{Channel: "regular", Version: "1.1.0"},
+						{Channel: "fast", Version: "2.0.0"},
+					},
+				},
+			},
+			kymas: &v1beta2.KymaList{
+				Items: []v1beta2.Kyma{
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-1", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "regular"}}},
+					},
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-2", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "fast"}}},
+					},
+				},
+			},
+			want: []*types.NamespacedName{{Name: "kyma-1", Namespace: "kcp-system"}},
+		},
+		{
+			name: "new channel added requeues kyma using that channel",
+			oldMRM: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels:   []v1beta2.ChannelVersionAssignment{{Channel: "regular", Version: "1.0.0"}},
+				},
+			},
+			newMRM: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels: []v1beta2.ChannelVersionAssignment{
+						{Channel: "regular", Version: "1.0.0"},
+						{Channel: "fast", Version: "2.0.0"},
+					},
+				},
+			},
+			kymas: &v1beta2.KymaList{
+				Items: []v1beta2.Kyma{
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-fast", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "fast"}}},
+					},
+				},
+			},
+			want: []*types.NamespacedName{{Name: "kyma-fast", Namespace: "kcp-system"}},
+		},
+		{
+			name: "deleted channel requeues kyma using that channel",
+			oldMRM: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels: []v1beta2.ChannelVersionAssignment{
+						{Channel: "regular", Version: "1.0.0"},
+						{Channel: "fast", Version: "2.0.0"},
+					},
+				},
+			},
+			newMRM: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels:   []v1beta2.ChannelVersionAssignment{{Channel: "regular", Version: "1.0.0"}},
+				},
+			},
+			kymas: &v1beta2.KymaList{
+				Items: []v1beta2.Kyma{
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-fast", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "fast"}}},
+					},
+				},
+			},
+			want: []*types.NamespacedName{{Name: "kyma-fast", Namespace: "kcp-system"}},
+		},
+		{
+			name: "multiple channels updated - multiple kymas requeued",
+			oldMRM: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels: []v1beta2.ChannelVersionAssignment{
+						{Channel: "regular", Version: "1.0.0"},
+						{Channel: "fast", Version: "2.0.0"},
+					},
+				},
+			},
+			newMRM: &v1beta2.ModuleReleaseMeta{
+				Spec: v1beta2.ModuleReleaseMetaSpec{
+					ModuleName: "module",
+					Channels: []v1beta2.ChannelVersionAssignment{
+						{Channel: "regular", Version: "1.1.0"},
+						{Channel: "fast", Version: "2.1.0"},
+					},
+				},
+			},
+			kymas: &v1beta2.KymaList{
+				Items: []v1beta2.Kyma{
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-1", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "regular"}}},
+					},
+					{
+						ObjectMeta: apimetav1.ObjectMeta{Name: "kyma-2", Namespace: "kcp-system"},
+						Status:     v1beta2.KymaStatus{Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "fast"}}},
+					},
+				},
 			},
 			want: []*types.NamespacedName{
-				{Name: "test-kyma-1", Namespace: "kcp-system"},
+				{Name: "kyma-1", Namespace: "kcp-system"},
+				{Name: "kyma-2", Namespace: "kcp-system"},
 			},
-		},
-		{
-			name: "Multiple updates affects multiple Kyma",
-			args: args{
-				kymas: &v1beta2.KymaList{
-					Items: []v1beta2.Kyma{
-						{
-							ObjectMeta: apimetav1.ObjectMeta{Name: "test-kyma-1", Namespace: "kcp-system"},
-							Status: v1beta2.KymaStatus{
-								Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "regular"}},
-							},
-						},
-						{
-							ObjectMeta: apimetav1.ObjectMeta{Name: "test-kyma-2", Namespace: "kcp-system"},
-							Status: v1beta2.KymaStatus{
-								Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "fast"}},
-							},
-						},
-					},
-				},
-				moduleName:       "module",
-				affectedChannels: []string{"regular", "fast"},
-			},
-			want: []*types.NamespacedName{
-				{Name: "test-kyma-1", Namespace: "kcp-system"},
-				{Name: "test-kyma-2", Namespace: "kcp-system"},
-			},
-		},
-		{
-			name: "Module name does not match - No Kyma affected",
-			args: args{
-				kymas: &v1beta2.KymaList{
-					Items: []v1beta2.Kyma{
-						{
-							ObjectMeta: apimetav1.ObjectMeta{Name: "test-kyma-1", Namespace: "kcp-system"},
-							Status: v1beta2.KymaStatus{
-								Modules: []v1beta2.ModuleStatus{{Name: "non-existent-module", Channel: "regular"}},
-							},
-						},
-					},
-				},
-				moduleName:       "module",
-				affectedChannels: []string{"regular"},
-			},
-			want: []*types.NamespacedName{},
-		},
-		{
-			name: "Module channel does not match - No Kyma affected",
-			args: args{
-				kymas: &v1beta2.KymaList{
-					Items: []v1beta2.Kyma{
-						{
-							ObjectMeta: apimetav1.ObjectMeta{Name: "test-kyma-1", Namespace: "kcp-system"},
-							Status: v1beta2.KymaStatus{
-								Modules: []v1beta2.ModuleStatus{{Name: "module", Channel: "regular"}},
-							},
-						},
-					},
-				},
-				moduleName:       "module",
-				affectedChannels: []string{"experimental"},
-			},
-			want: []*types.NamespacedName{},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := events.GetAffectedKymas(tt.args.kymas, tt.args.moduleName,
-				tt.args.affectedChannels); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetAffectedKymas() = %v, want %v", got, tt.want)
-			}
+			got := events.AffectedKymasOnUpdate(tt.oldMRM, tt.newMRM, tt.kymas)
+			sortNamespacedNames(got)
+			sortNamespacedNames(tt.want)
+			require.Equal(t, tt.want, got)
 		})
 	}
+}
+
+func sortNamespacedNames(names []*types.NamespacedName) {
+	sort.Slice(names, func(i, j int) bool {
+		return names[i].Name < names[j].Name
+	})
 }
