63e85d0207ae485d8157c214520ba2e0c91d7c28
c-pius <c.schwaegerl@sap.com>
2026-05-18 12:53:25 +0200

    module-deletion-with-only-default-cr

---
+++ tests/e2e/module_deletion_with_only_default_cr_test.mk
@@ -0,0 +1,36 @@
+.DEFAULT_GOAL := test
+.PHONY: test $(MAKECMDGOALS)
+
+include $(dir $(abspath $(lastword $(MAKEFILE_LIST))))e2e.common.mk
+
+.PHONY: klm-patch
+klm-patch:
+	@echo "::group::KLM patch"
+	@echo "No test-specific KLM patches"
+	@echo "::endgroup::"
+
+.PHONY: module-setup
+module-setup: module-setup-latest
+	@echo "::group::Test-specific ModuleReleaseMeta setup"
+	@export PATH=$(LOCALBIN):$$PATH
+	$(SCRIPTS_DIR)/deploy_modulereleasemeta.sh $(MODULE_NAME) regular:$(MODULE_DEPLOYABLE_VERSION)
+	@echo "::endgroup::"
+
+.PHONY: test-run
+test-run: log-tool-versions
+	@echo "::group::Setting kubeconfig variables"
+	@export KCP_KUBECONFIG=$(shell k3d kubeconfig write kcp)
+	@export SKR_KUBECONFIG=$(shell k3d kubeconfig write skr)
+	@echo "::endgroup::"
+
+	@echo "::group::E2E test: Non Blocking Module Deletion With Only Default CR"
+	@export PATH=$(LOCALBIN):$$PATH
+	@pushd $(E2E_TESTS_DIR) > /dev/null
+	set +e; $(GO) test -timeout 20m -ginkgo.v -ginkgo.focus "Non Blocking Module Deletion With Only Default CR"; status=$$?; set -e
+	@popd > /dev/null
+	@echo "::endgroup::"
+	exit $${status}
+
+
+.PHONY: test
+test: create-clusters klm-patch deploy-klm module-setup test-run
