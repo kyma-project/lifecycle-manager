ccd5d6d630203508550938bc704ac904dd50de33
I507792 <c.schwaegerl@sap.com>
2026-06-18 14:57:31 +0200

    add deplyoer to blocking test

--- tests/e2e/block_non_deployer_module_image_pull_secret_injection_test.mk
+++ tests/e2e/block_non_deployer_module_image_pull_secret_injection_test.mk
@@ -4,6 +4,7 @@
 include $(dir $(abspath $(lastword $(MAKEFILE_LIST))))e2e.common.mk
 
 MODULE_NAME := not-deployer
+DEPLOYER_MODULE_NAME := deployer
 GLOBAL_ACCOUNT_ID ?= a1c1d2e3-4a5b-6c7d-8e9f-0a1b2c3d4e5f
 
 .PHONY: klm-patch
@@ -12,7 +13,7 @@ klm-patch: kustomize-install
 	@export PATH=$(LOCALBIN):$$PATH
 	@pushd $(LIFECYCLE_MANAGER_DIR)/config/watcher_local_test > /dev/null
 	kustomize edit add patch --kind Deployment --patch \
-		'[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--restricted-default-modules=$(MODULE_NAME)"}]'
+		'[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--restricted-default-modules=deployer,$(MODULE_NAME)"}]'
 	@popd > /dev/null
 	@echo "::endgroup::"
 
@@ -21,6 +22,7 @@ module-setup:
 	@echo "::group::Test-specific module metadata setup"
 	@export PATH=$(LOCALBIN):$$PATH
 	@pushd $(TEMPLATE_OPERATOR_DIR) > /dev/null
+# not-deployer module contains fullfills the criteria for image pull secret injection;
 	$(SCRIPTS_DIR)/deploy_moduletemplate_e2e.sh \
 		--module-name $(MODULE_NAME) \
 		--version $(MODULE_OLDER_VERSION) \
@@ -29,6 +31,17 @@ module-setup:
 		--additional-resources $(E2E_TESTS_DIR)/testdata/skr-deployer-image-pull-secret.yaml
 	KEEP_FILE=true $(SCRIPTS_DIR)/deploy_modulereleasemeta.sh $(MODULE_NAME) fast:$(MODULE_OLDER_VERSION) regular:$(MODULE_OLDER_VERSION)
 	yq -i '.spec.kymaSelector.matchExpressions = [{"key": "kyma-project.io/global-account-id", "operator": "In", "values": ["$(GLOBAL_ACCOUNT_ID)"]}]' module-release-meta.yaml
+	kubectl apply -f module-release-meta.yaml
+	rm -f module-release-meta.yaml
+# deployer module is required to exist because it is configured as a restricted default module;
+# its kymaSelector uses a non-matching global-account-id so it is not installed for the test Kyma CR
+	$(SCRIPTS_DIR)/deploy_moduletemplate_e2e.sh \
+		--module-name $(DEPLOYER_MODULE_NAME) \
+		--version $(MODULE_OLDER_VERSION) \
+		--deployment-name $(MODULE_DEPLOYMENT_OLDER_VERSION) \
+		--deployable-version $(MODULE_DEPLOYABLE_VERSION)
+	KEEP_FILE=true $(SCRIPTS_DIR)/deploy_modulereleasemeta.sh $(DEPLOYER_MODULE_NAME) fast:$(MODULE_OLDER_VERSION) regular:$(MODULE_OLDER_VERSION)
+	yq -i '.spec.kymaSelector.matchExpressions = [{"key": "kyma-project.io/global-account-id", "operator": "In", "values": ["00000000-0000-0000-0000-000000000000"]}]' module-release-meta.yaml
 	kubectl apply -f module-release-meta.yaml
 	rm -f module-release-meta.yaml
 # KCP secret: data that should NOT be injected because the module is not named "deployer"
