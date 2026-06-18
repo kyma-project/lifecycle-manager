9c6fdad2b31d1c67d1395fc69897842840db9426
I507792 <c.schwaegerl@sap.com>
2026-06-18 14:11:30 +0200

    block-non-deployer-module-image-pull-secret-sync

---
+++ tests/e2e/block_non_deployer_module_image_pull_secret_sync_test.go
@@ -0,0 +1,57 @@
+package e2e_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/kyma-project/lifecycle-manager/api/v1beta2"
+
+	. "github.com/kyma-project/lifecycle-manager/pkg/testutils"
+	. "github.com/kyma-project/lifecycle-manager/tests/e2e/commontestutils"
+)
+
+const notDeployerModuleName = "not-deployer"
+
+var _ = Describe("Block Non Deployer Module Image Pull Secret Sync", Ordered, func() {
+	kyma := NewKymaWithNamespaceName("kyma-sample", ControlPlaneNamespace, v1beta2.DefaultChannel)
+	// this global account id is added to the module release meta in the Makefile
+	kyma.Labels["kyma-project.io/global-account-id"] = "a1c1d2e3-4a5b-6c7d-8e9f-0a1b2c3d4e5f"
+	moduleCR := NewTestModuleCR(RemoteNamespace)
+
+	InitEmptyKymaBeforeAll(kyma)
+	CleanupKymaAfterAll(kyma)
+
+	Context("Given SKR Cluster", func() {
+		It("Then the not-deployer module is installed as a restricted default module", func() {
+			Eventually(ContainsModuleInSpec).
+				WithContext(ctx).
+				WithArguments(kcpClient, kyma.GetName(), kyma.GetNamespace(), notDeployerModuleName).
+				Should(Succeed())
+			By("And the Module CR has been installed on the SKR cluster")
+			Eventually(ModuleCRExists).
+				WithContext(ctx).
+				WithArguments(skrClient, moduleCR).
+				Should(Succeed())
+			By("And the Module Operator Deployment is ready on the SKR cluster")
+			Eventually(DeploymentIsReady).
+				WithContext(ctx).
+				WithArguments(skrClient, ModuleDeploymentNameInOlderVersion, TestModuleResourceNamespace).
+				Should(Succeed())
+		})
+
+		It("Then the annotated image-pull-secret on the SKR is not patched", func() {
+			Consistently(SecretDataEquals).
+				WithContext(ctx).
+				WithArguments(
+					skrClient,
+					"image-pull-secret",
+					TestModuleResourceNamespace,
+					".dockerconfigjson",
+					[]byte(`{"auths": {}}`+"\n"),
+				).
+				WithTimeout(ConsistentDuration).
+				WithPolling(interval).
+				Should(Succeed())
+		})
+	})
+})
