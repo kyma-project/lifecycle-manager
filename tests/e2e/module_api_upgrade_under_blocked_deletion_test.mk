b9c07b855ceacca44dc3cc7a94d3eb1249ac718d
Amritanshu Sikdar <amritanshu.sikdar@sap.com>
2026-06-26 14:30:35 +0200

    remove unneeded lines

--- tests/e2e/module_api_upgrade_under_blocked_deletion_test.mk
+++ tests/e2e/module_api_upgrade_under_blocked_deletion_test.mk
@@ -14,8 +14,6 @@ module-setup-in-newer-version-e2e-with-crd-upgrade:
 	@echo "::group::Module setup with $(MODULE_NEWER_VERSION) and CRD v1beta1 upgrade"
 	@export PATH=$(LOCALBIN):$$PATH
 	@pushd $(TEMPLATE_OPERATOR_DIR) > /dev/null
-	yq eval '.apiVersion = "operator.kyma-project.io/v1alpha1"' -i config/samples/default-sample-cr.yaml
-	cp $(SCRIPTS_DIR)/deploy_moduletemplate.sh .
 	yq eval '.images[0].newTag = "$(MODULE_NEWER_VERSION)"' -i config/manager/deployment/kustomization.yaml
 	make build-manifests
 	yq eval '(. | select(.kind == "Deployment") | .metadata.name) = "$(MODULE_DEPLOYMENT_NEWER_VERSION)"' -i template-operator.yaml
