bf257c128038226d12aa0a139979ce30b4c5e3b8
Amritanshu Sikdar <amritanshu.sikdar@sap.com>
2026-06-26 16:06:59 +0200

    feat(E2E from Makefile): Port Module API Upgrade Under Blocked Deletion Test (#3389)
    * conform existing module_api_upgrade_under_blocked_deletion test to new
    e2e standard
    
    * add missing yq surgery
    
    * adjust github workflow accordingly
    
    * remove `module-setup-latest` from target prerequisite
    
    * use already existing targets from `e2e.common.mk`
    remove *_E2E aliases via adjusted ACs
    
    * remove unneeded lines
    
    * rebase main
    
    * satisfy linter

---
+++ tests/e2e/module_api_upgrade_under_blocked_deletion_test.mk
@@ -0,0 +1,51 @@
+.DEFAULT_GOAL := test
+.PHONY: test $(MAKECMDGOALS)
+
+include $(dir $(abspath $(lastword $(MAKEFILE_LIST))))e2e.common.mk
+
+.PHONY: klm-patch
+klm-patch:
+	@echo "::group::KLM patch"
+	@echo "No test-specific KLM patches"
+	@echo "::endgroup::"
+
+.PHONY: module-setup-in-newer-version-e2e-with-crd-upgrade
+module-setup-in-newer-version-e2e-with-crd-upgrade:
+	@echo "::group::Module setup with $(MODULE_NEWER_VERSION) and CRD v1beta1 upgrade"
+	@export PATH=$(LOCALBIN):$$PATH
+	@pushd $(TEMPLATE_OPERATOR_DIR) > /dev/null
+	yq eval '.images[0].newTag = "$(MODULE_NEWER_VERSION)"' -i config/manager/deployment/kustomization.yaml
+	make build-manifests
+	yq eval '(. | select(.kind == "Deployment") | .metadata.name) = "$(MODULE_DEPLOYMENT_NEWER_VERSION)"' -i template-operator.yaml
+	crd_selector='select(.kind == "CustomResourceDefinition" and .metadata.name == "samples.operator.kyma-project.io")'
+	yq eval "$${crd_selector} |= (.spec.versions += [{\"name\": \"v1beta1\", \"served\": true, \"storage\": true, \"schema\": .spec.versions[0].schema}])" -i template-operator.yaml
+	yq eval "($${crd_selector} | .spec.versions[] | select(.name == \"v1alpha1\")).storage = false" -i template-operator.yaml
+	yq eval '.apiVersion = "operator.kyma-project.io/v1beta1"' -i config/samples/default-sample-cr.yaml
+	./deploy_moduletemplate.sh $(MODULE_NAME) $(MODULE_NEWER_VERSION) $(MODULE_DEPLOYABLE_VERSION) true false true false
+	@popd > /dev/null
+	@echo "::endgroup::"
+
+.PHONY: module-setup
+module-setup: module-setup-in-older-version module-setup-in-newer-version-e2e-with-crd-upgrade
+	@echo "::group::Test-specific module metadata setup"
+	@export PATH=$(LOCALBIN):$$PATH
+	$(SCRIPTS_DIR)/deploy_modulereleasemeta.sh $(MODULE_NAME) fast:$(MODULE_NEWER_VERSION) regular:$(MODULE_OLDER_VERSION)
+	@echo "::endgroup::"
+
+.PHONY: test-run
+test-run: log-tool-versions
+	@echo "::group::Setting kubeconfig variables"
+	@export KCP_KUBECONFIG=$(shell k3d kubeconfig write kcp)
+	@export SKR_KUBECONFIG=$(shell k3d kubeconfig write skr)
+	@echo "::endgroup::"
+
+	@echo "::group::E2E test: Module API Upgrade Under Blocked Deletion"
+	@export PATH=$(LOCALBIN):$$PATH
+	@pushd $(E2E_TESTS_DIR) > /dev/null
+	set +e; $(GO) test -timeout 20m -ginkgo.v -ginkgo.focus "Module API Upgrade Under Blocked Deletion"; status=$$?; set -e
+	@popd > /dev/null
+	@echo "::endgroup::"
+	exit $${status}
+
+.PHONY: test
+test: create-clusters klm-patch deploy-klm module-setup test-run
