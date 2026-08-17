89c23f7c5d08bbd86554940a07ebc6a9cfced5ef
Raj <me@hukumraj-singh-deora.party>
2026-08-17 11:57:28 +0200

    test: isolate mandatory-module spec to assert aggregate Warning state

--- tests/integration/controller/kyma/kyma_module_enable_test.go
+++ tests/integration/controller/kyma/kyma_module_enable_test.go
@@ -77,7 +77,34 @@ var _ = Describe("Given kyma CR with invalid module enabled", Ordered, func() {
 		Eventually(expectKymaStatusModules(ctx, kyma, module.Name, shared.StateError), Timeout,
 			Interval).Should(Succeed())
 	})
-	It("When enabling a mandatory module, expect module status in Warning state", func() {
+})
+
+var _ = Describe("Given kyma CR with a mandatory module enabled", Ordered, func() {
+	kyma := NewTestKyma("mandatory-kyma")
+	skrKyma := NewSKRKyma()
+	objTracker := &deletionTracker{}
+	var skrClient client.Client
+	var err error
+	BeforeAll(func() {
+		Eventually(CreateCR, Timeout, Interval).
+			WithContext(ctx).
+			WithArguments(kcpClient, kyma).Should(Succeed())
+		Eventually(func() error {
+			skrClient, err = testSkrContextFactory.Get(kyma.GetNamespacedName())
+			return err
+		}, Timeout, Interval).Should(Succeed())
+	})
+	AfterAll(func() {
+		Eventually(DeleteCR, Timeout, Interval).
+			WithContext(ctx).
+			WithArguments(kcpClient, kyma).Should(Succeed())
+
+		Eventually(objTracker.tryDeleteAll, Timeout, Interval).
+			WithContext(ctx).
+			WithArguments(kcpClient).
+			Should(Succeed())
+	})
+	It("When enabling a mandatory module, expect Kyma and module status in Warning state", func() {
 		const mandatoryModuleName = "mandatory-enabled"
 		DeployMandatoryModuleTemplate(ctx, kcpClient, mandatoryModuleName, ver110, objTracker)
 		module := NewTestModuleWithFixName(mandatoryModuleName, v1beta2.DefaultChannel, "")
